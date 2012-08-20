48d115a139a7fe9eb7ae0885f7470d347c8e428a
Naomi Dushay <ndushay@stanford.edu>
2012-08-20 15:05:12 -0700

    revised japanese searches per vitus email

--- spec/japanese_single_char_spec.rb
+++ spec/japanese_single_char_spec.rb
@@ -2,19 +2,20 @@
 require 'spec_helper'
 require 'rspec-solr'
 
+# need these to work -- CJKBigram approach could interfere with that
 describe "Japanese: Single Character Searches", :japanese => true, :fixme => true do
   
-  it "title  乱 should get results" do
+  it "title  乱 (modern) should get results" do
     resp = solr_response({'q'=>'乱', 'fl'=>'id,vern_title_245a_display', 'qt'=>'search_title', 'facet'=>false}) 
     resp.should include('vern_title_245a_display'=>'乱').in_first(4).documents 
     resp.should include("6260985") # famous movie by Akira Kurosawa
     resp.should have_at_least(695).documents # 4 in prod, 695 title in soc
   end
-  
-  it "title  禪 (zen) should get results" do
-    resp = solr_response({'q'=>'禪', 'fl'=>'id,vern_title_245a_display', 'qt'=>'search_title', 'facet'=>false}) 
-    resp.should include('vern_title_245a_display'=>'禪').in_first(6).documents 
-    resp.should include("6667691") 
+
+  it "title Zen  禅 (modern) should get results" do
+    resp = solr_response({'q'=>'禅', 'fl'=>'id,vern_title_245a_display', 'qt'=>'search_title', 'facet'=>false, }) 
+    resp.should include('vern_title_245a_display'=>'禅').in_first(6).documents 
+    resp.should include("4193363") 
     resp.should have_at_least(890).documents # 6 in prod, 890 title in soc
   end
   
--- spec/japanese_traditional_modern_characters_spec.rb
+++ spec/japanese_traditional_modern_characters_spec.rb
@@ -4,7 +4,7 @@ require 'rspec-solr'
 
 describe "Japanese: Traditional and Modern Characters", :japanese => true, :fixme => true do
 
-  it "(bukkyogaku) Traditional chars 佛教學 should get the same results as simplified chars 仏教学" do
+  it "(bukkyogaku) Traditional (Han) chars 佛教學 should get the same results as simplified/kanji chars 仏教学" do
     resp = solr_resp_doc_ids_only({'q'=>'bukkyogaku'}) # 49 in prod
     resp.should have_at_least(40).documents
     resp.should have_at_most(60).documents
@@ -14,12 +14,49 @@ describe "Japanese: Traditional and Modern Characters", :japanese => true, :fixm
     resp.should have_the_same_number_of_results_as(solr_resp_doc_ids_only({'q'=>'佛教學'})) # 2 in prod, 279 in soc
   end
   
-  it "(teikoku) Traditional chars 帝國 should get the same results as simplified chars 帝国" do
+  it "(teikoku) Traditional chars 帝國 should get the same results as simplified/kanji chars 帝国" do
     resp = solr_resp_doc_ids_only({'q'=>'teikoku'}) # 1560 in prod, 1559 in soc
     resp.should have_at_least(1550).documents
-    resp.should have_the_same_number_of_results_as(solr_resp_doc_ids_only({'q'=>'帝國'})) # 10 in prod, 1593 in prod
-    resp.should have_the_same_number_of_results_as(solr_resp_doc_ids_only({'q'=>'帝国'})) # 58 in prod, 1593 in prod
-    # simplified  帝国  has some in Korean and Chinese ... is that wrong?
+    resp.should have_the_same_number_of_results_as(solr_resp_doc_ids_only({'q'=>'帝國'})) # 10 in prod, 1593 in soc
+    resp.should have_the_same_number_of_results_as(solr_resp_doc_ids_only({'q'=>'帝国'})) # 58 in prod, 1593 in soc
+    # these results have some in Korean and Chinese, too
+    resp.should have_more_documents_than(solr_resp_doc_ids_only({'q'=>'帝國', 'fq'=>'language:Japanese'})) # 6 in prod, 1347 in soc
+    # 'teikoku'  and lang Japanese   794 in prod, 1494 in soc
   end
+  
+  it "(The South Manchuria Railway Company) 南滿洲鐵道株式會社 should get the same results as 南満州鉄道株式会社" do
+    #  滿 is traditional, 満 is the modern form; 鐵 is traditional, and 鉄 is the modern form)
+    # (minami manshu tetsudo kabushiki kaisha) in latin?
+    resp = solr_resp_doc_ids_only({'q'=>'南滿洲鐵道株式會社'}) # 513 in prod, 564 in soc
+    resp.should have_at_least(881).documents
+    resp.should have_the_same_number_of_results_as(solr_resp_doc_ids_only({'q'=>'南満州鉄道株式会社'})) # 48 in prod, 52 in soc
+  end
+  
+  it "modern char author name  けいちゅう should get the results with traditional author name  契沖" do
+    resp = solr_resp_doc_ids_only({'q'=>'契沖', 'qt'=>'search_author'}) # 5 in prod, 5 in soc
+    expected_results = ["6675613", "6675393", "7191966", "6274534", "4783602"]
+    resp.should include(expected_results)
+    resp2 = solr_resp_doc_ids_only({'q'=>'けいちゅう'}) # 0 in prod, 1 in soc
+    resp2.should include(expected_results)  # has author name  契沖
+  end
+  
+  context "Single character searches" do
+        
+    it "title  乱 (modern) should get same results as  亂 (trad)" do
+      resp = solr_resp_doc_ids_only({'q'=>'乱', 'qt'=>'search_title', 'fq'=>'language:Japanese'}) 
+      resp.should have_at_least(320).documents # 4 in prod, 323 in soc
+      resp.should include("6260985") # famous movie by Akira Kurosawa
+      resp.should include("4176905") # trad
+      resp.should have_the_same_number_of_results_as(solr_resp_doc_ids_only({'q'=>'亂', 'qt'=>'search_title', 'fq'=>'language:Japanese'})) # 0 in prod, 323 in soc 
+    end
 
+    it "title Zen  禪 (trad) should get same results as  禅 (modern)" do
+      resp = solr_resp_doc_ids_only({'q'=>'禪', 'qt'=>'search_title', 'fq'=>'language:Japanese'}) 
+      resp.should have_at_least(425).documents # 1 in prod, 435 in soc
+      resp.should include("6667691")  # trad
+      resp.should include("4193363")  # modern
+      resp.should have_the_same_number_of_results_as(solr_resp_doc_ids_only({'q'=>'禅', 'qt'=>'search_title', 'fq'=>'language:Japanese'})) # 5 in prod, 435 in soc 
+    end
+  end
+  
 end
