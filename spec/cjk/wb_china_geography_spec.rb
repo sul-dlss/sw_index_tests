12f1d7b78416e3703e2b43e1a1759b28d38eafe9
Naomi Dushay <ndushay@stanford.edu>
2012-08-29 17:46:07 -0700

    adding info to two wb specs

--- spec/cjk/wb_china_geography_spec.rb
+++ spec/cjk/wb_china_geography_spec.rb
@@ -4,38 +4,72 @@ require 'rspec-solr'
 
 describe "Chinese: Word Breaks - a place in China geography", :chinese => true, :fixme => true, :wordbreak => true do
 
-  # cjk1 (indexUnigrams true, agenphraseq false)     searchworks-dev
-  # cjk3 (indexUnigrams false, agenphraseq false)   
-  # cjk6
-
   context " 郑州 (a place in China)  地理 (geography)" do
+    it "郑州地理 should have good search results" do
+      resp = solr_resp_doc_ids_only({'q'=>'郑州地理', 'qt'=>'search_title'}) 
+      resp.should include("9612993").as_first.result  
+      resp.should have_at_most(1).document
+      # soc:  
+      # cjk1 (bigrams and unigrams):   2630 results
+      # cjk3 (bigrams only):           2630 results
+      # cjk7 (unigrams only):         39598
+      # cjk6cn (chinese dict only):    2235 
+      # cjk6ja (japanese dict only):   7668
+    end
+    
+    it "郑州 地理 as two terms should have good search results" do
+      resp = solr_resp_doc_ids_only({'q'=>'郑州 地理', 'qt'=>'search_title'})
+      resp.should include("9612993").as_first.result
+      resp.should have_at_most(1).document
+      
+      # soc: 
+      # cjk1 (bigrams and unigrams): 2 results
+      #    4203541   place 260a; geography 245a, 490a, 830a
+      #    5809974   place 260a; geography 245a
+      # cjk3 (bigrams only):           2 results
+      # cjk7 (unigrams only):       1323
+      # cjk6cn (chinese dict only):    2   (same as cjk1)
+      # cjk6ja (japanese dict only):  81
+    end
+    
     context "title search" do
+      it "郑州地理 should have good search results" do
+        resp = solr_resp_doc_ids_only({'q'=>'郑州地理', 'qt'=>'search_title'}) 
+        resp.should include("9612993").as_first.result  
+        resp.should have_at_most(1).document
+        # soc:  1 result
+        #    9612993     郑州地理 never;  郑州  245a, 520a, 651a;   地理  245a (but two chars between), 520a
+        #  (not avail in dev)
+        # cjk1 (bigrams and unigrams):   2286 results
+        #    6324746     郑州地理 never;  郑州 never;  地理 245a;   州地 (nonsense) 245a  (abcd - never, ab - never, cd - 245a; bc - 245a, bcd - 245a)
+        #    5631095
+        #    4188978
+        #    6827354
+        #    8720366
+        #    4822107
+        #    4703364
+        #    ...
+        # cjk3 (bigrams only):         2286
+        # cjk7 (unigrams only):       31,543
+        # cjk6cn (chinese dict only):  2,016 
+        # cjk6ja (japanese dict only): 5,298
+      end
+      
       it "郑州 地理 as two terms should have good search results" do
         resp = solr_resp_doc_ids_only({'q'=>'郑州 地理', 'qt'=>'search_title'})
         resp.should include("9612993").as_first.result
-        resp.should have_at_most(1).documents
+        resp.should have_at_most(1).document
+        
+        # soc:  1 result
+        #    9612993     郑州地理 never;  郑州  245a, 520a, 651a;   地理  245a (but two chars between), 520a
+        #   (not avail in dev)
+        # cjk1 (bigrams and unigrams):   0 results
+        # cjk3 (bigrams only):           0 results
+        # cjk7 (unigrams only):         746
+        # cjk6cn (chinese dict only):    0 results
+        # cjk6ja (japanese dict only):   57 results
       end
-      it "郑州地理 should have good search results" do
-        resp = solr_resp_doc_ids_only({'q'=>'郑州地理', 'qt'=>'search_title'}) 
-        resp.should include("9612993").as_first.result  # it's second
-      end
-      
-      # 郑州地理 title search
-      #  socrates:  1 result:  
-      #    9612993     郑州地理 never;  郑州  245a, 520a, 651a;   地理  245a (but two chars between), 520a
-      #  cjk1:    2305 results
-      #    6324746     郑州地理 never;  郑州 never;  地理 245a;   州地 (nonsense) 245a  (abcd - never, ab - never, cd - 245a; bc - 245a, bcd - 245a)
-      #    9612993 is 2nd
-      #    ...
-      #    
-      #  郑州  地理 title search
-      #  socrates:  1 result:   9612993
-      #    cjk1:   1 result
-      # 
-      #   郑州地理  title search
-      
-    end
-    
-  end
+    end # context title_search
+  end # context  (a place in China) (geography)
     
 end
--- spec/cjk/wb_unigram_spec.rb
+++ spec/cjk/wb_unigram_spec.rb
@@ -11,20 +11,96 @@ describe "Chinese: Word Breaks - Unigrams", :chinese => true, :fixme => true, :w
   # cjk5 (add CJKBigram filter to textNoStem, indexUnigrams true, agenphraseq false)   searchworks-dev:3001
   
   context "舊小說 (old fiction) becomes  舊 (unigram) and  小說 (bigram)" do
-    # 舊 (old)  小說 (fiction)
-    # symph title search 11 records
-    # 
-    # cjk1, 4 (no space) everything search 3102 records;   cjk2, 3  3 records  (3 records have 3 chars together)
-    # cjk1, 2, 3, 4  舊  小說  0 records
-    # 
-    # no space
-    # space
-    # phrase no space
-    # phrase space
-    # AND query
-    # OR query
-    # positionfilter ...
-    # overlapping tokens ...   what if they didn't overlap and were ANDed
+
+    it "舊小說 (no spaces) should have good search results" do
+      resp = solr_resp_doc_ids_only({'q'=>'舊小說', 'rows'=>'20'}) 
+      resp.should include(["9262744", "6797638", "6695967"]).as_first(3).results
+      resp.should include(["6695904", "6696790"]).in_first(5).documents # both words in 245a
+      resp.should include(["7198256", "6699444"]).in_first(8).documents  # both words in 245
+      # soc:  24 results
+      # cjk1 (bigrams and unigrams):  3,087 results
+      # cjk3 (bigrams only):          3,087 results
+      # cjk7 (unigrams only):        27,879 results
+      # cjk6cn (chinese dict only):  4,980 results
+      # cjk6ja (japanese dict only): 19,599 results
+    end
+    it "舊 小說 (space) should have good search results" do
+      resp = solr_resp_doc_ids_only({'q'=>'舊 小說', 'rows'=>'20'}) 
+      resp.should include(["9262744", "6797638", "6695967"]).as_first(3).results
+      resp.should include(["6695904", "6696790"]).in_first(5).documents # both words in 245a
+      resp.should include(["7198256", "6699444"]).in_first(8).documents  # both words in 245
+      # soc:  35 results
+      # cjk1 (bigrams and unigrams):  0 results 
+      # cjk3 (bigrams only):          0 results
+      # cjk7 (unigrams only):        73 results
+      # cjk6cn (chinese dict only):  10 results
+      # cjk6ja (japanese dict only): 68 results
+    end
+    context "title search" do
+      it "舊小說 (no spaces) should have good search results" do
+        resp = solr_resp_doc_ids_only({'q'=>'舊小說', 'rows'=>'20', 'qt'=>'search_title'}) 
+        resp.should include(["9262744", "6797638", "6695967"]).as_first(3).results
+        resp.should include(["6695904", "6696790"]).in_first(5).documents # both words in 245a
+        resp.should include(["7198256", "6699444"]).in_first(8).documents  # both words in 245
+        # soc:  11 results
+        #   8834455  - ?? translated to simplified??
+        #   7198256 - old 245b; fiction: 245a
+        #   6793760 - ??
+        #   6288832 - old 505t; fiction 505t x2
+        #   4192734 - ??
+        #   9262744 - old fiction: 245a
+        #   6797638 - old fiction: 245a
+        #   6695967 - old fiction: 245a
+        #   6695904 - fiction 245a; old 245a
+        #   6699444 - old 245a; fiction 245b
+        #   6696790 - old 245a; fiction 245a
+        # 
+        # cjk1 (bigrams and unigrams):  2,862 results  
+        # cjk3 (bigrams only):          2,862
+        # cjk7 (unigrams only):        19,791 results
+        # cjk6cn (chinese dict only):   4,576 results
+        # cjk6ja (japanese dict only): 16,451 results
+      end  
+      it "舊 小說 (space) should have good search results" do
+        resp = solr_resp_doc_ids_only({'q'=>'舊 小說', 'rows'=>'20', 'qt'=>'search_title'}) 
+        resp.should include(["9262744", "6797638", "6695967"]).as_first(3).results
+        resp.should include(["6695904", "6696790"]).in_first(5).documents # both words in 245a
+        resp.should include(["7198256", "6699444"]).in_first(8).documents  # both words in 245
+        # soc:  14 results
+        #   8834455  - ?? translated to simplified??
+        # NEW 7699186 - ??
+        #   7198256 - old 245b; fiction: 245a
+        #   6793760 - ??
+        #   6288832 - old 505t; fiction 505t x2
+        #   4192734 - ??
+        # NEW 6204747 - old 245a; fiction 490a; 830a
+        #   9262744 - old fiction: 245a
+        #   6797638 - old fiction: 245a
+        #   6695967 - old fiction: 245a
+        #   6695904 - fiction 245a; old 245a
+        #   6699444 - old 245a; fiction 245b
+        # NEW 6698466 - old 245a; fiction 490a, 830a
+        #   6696790 - old 245a; fiction 245a
+        # 
+        # cjk1 (bigrams and unigrams):  0 results <--  what???
+        # cjk3 (bigrams only):          0 results
+        # cjk7 (unigrams only):        46 results
+        # cjk6cn (chinese dict only):   9 results
+        #    8834455
+        #    9262744
+        #    6797638
+        #    6695967
+        #    6793760
+        #    6699444
+        #    6698466
+        #    7198256
+        #    6288832
+        # 
+        # cjk6ja (japanese dict only): 46 results
+      end
+      
+    end
+    
   end
   
   context "婦女與文學 becomes  婦女 (women)  與 (and)   文學 (literature)" do
