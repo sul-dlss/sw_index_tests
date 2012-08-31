b7c391815b0729ca73c7aece828b5e243856e1d4
Naomi Dushay <ndushay@stanford.edu>
2012-08-31 14:30:21 -0700

    flesh out cjk word break specs after spinning up some experimental indexes

--- spec/cjk/wb_china_geography_spec.rb
+++ spec/cjk/wb_china_geography_spec.rb
@@ -9,12 +9,15 @@ describe "Chinese: Word Breaks - a place in China geography", :chinese => true, 
       resp = solr_resp_doc_ids_only({'q'=>'郑州地理', 'qt'=>'search_title'}) 
       resp.should include("9612993").as_first.result  
       resp.should have_at_most(1).document
-      # soc:  
-      # cjk1 (bigrams and unigrams):   2630 results
-      # cjk3 (bigrams only):           2630 results
+      # soc:                              2
+      #   9612993
+      #   5456407
+      # cjk1 (bigrams and unigrams):   2630 
+      # cjk3 (bigrams only):           2630 
       # cjk7 (unigrams only):         39598
       # cjk6cn (chinese dict only):    2235 
       # cjk6ja (japanese dict only):   7668
+      # cjk8 (chinese dict no pos):    2235
     end
     
     it "郑州 地理 as two terms should have good search results" do
@@ -22,14 +25,23 @@ describe "Chinese: Word Breaks - a place in China geography", :chinese => true, 
       resp.should include("9612993").as_first.result
       resp.should have_at_most(1).document
       
-      # soc: 
-      # cjk1 (bigrams and unigrams): 2 results
+      # soc:                           8
+      #     9612993
+      #     7833848
+      #     5809974
+      #     4808042
+      #     4203541
+      #     4197620
+      #     5456407
+      #     6704074
+      # cjk1 (bigrams and unigrams):   2
       #    4203541   place 260a; geography 245a, 490a, 830a
       #    5809974   place 260a; geography 245a
-      # cjk3 (bigrams only):           2 results
+      # cjk3 (bigrams only):           2 
       # cjk7 (unigrams only):       1323
       # cjk6cn (chinese dict only):    2   (same as cjk1)
       # cjk6ja (japanese dict only):  81
+      # cjk8 (chinese dict no pos):    2
     end
     
     context "title search" do
@@ -37,10 +49,10 @@ describe "Chinese: Word Breaks - a place in China geography", :chinese => true, 
         resp = solr_resp_doc_ids_only({'q'=>'郑州地理', 'qt'=>'search_title'}) 
         resp.should include("9612993").as_first.result  
         resp.should have_at_most(1).document
-        # soc:  1 result
+        # soc:                       1 result
         #    9612993     郑州地理 never;  郑州  245a, 520a, 651a;   地理  245a (but two chars between), 520a
         #  (not avail in dev)
-        # cjk1 (bigrams and unigrams):   2286 results
+        # cjk1 (bigrams and unigrams): 2,286
         #    6324746     郑州地理 never;  郑州 never;  地理 245a;   州地 (nonsense) 245a  (abcd - never, ab - never, cd - 245a; bc - 245a, bcd - 245a)
         #    5631095
         #    4188978
@@ -49,10 +61,11 @@ describe "Chinese: Word Breaks - a place in China geography", :chinese => true, 
         #    4822107
         #    4703364
         #    ...
-        # cjk3 (bigrams only):         2286
+        # cjk3 (bigrams only):         2,286
         # cjk7 (unigrams only):       31,543
         # cjk6cn (chinese dict only):  2,016 
         # cjk6ja (japanese dict only): 5,298
+        # cjk8 (chinese dict no pos):  2,016
       end
       
       it "郑州 地理 as two terms should have good search results" do
@@ -63,11 +76,12 @@ describe "Chinese: Word Breaks - a place in China geography", :chinese => true, 
         # soc:  1 result
         #    9612993     郑州地理 never;  郑州  245a, 520a, 651a;   地理  245a (but two chars between), 520a
         #   (not avail in dev)
-        # cjk1 (bigrams and unigrams):   0 results
-        # cjk3 (bigrams only):           0 results
-        # cjk7 (unigrams only):         746
-        # cjk6cn (chinese dict only):    0 results
-        # cjk6ja (japanese dict only):   57 results
+        # cjk1 (bigrams and unigrams):   0
+        # cjk3 (bigrams only):           0 
+        # cjk7 (unigrams only):        746
+        # cjk6cn (chinese dict only):    0
+        # cjk6ja (japanese dict only):  57 
+        # cjk8 (chinese dict no pos):    0
       end
     end # context title_search
   end # context  (a place in China) (geography)
--- spec/cjk/wb_unigram_spec.rb
+++ spec/cjk/wb_unigram_spec.rb
@@ -17,24 +17,26 @@ describe "Chinese: Word Breaks - Unigrams", :chinese => true, :fixme => true, :w
       resp.should include(["9262744", "6797638", "6695967"]).as_first(3).results
       resp.should include(["6695904", "6696790"]).in_first(5).documents # both words in 245a
       resp.should include(["7198256", "6699444"]).in_first(8).documents  # both words in 245
-      # soc:  24 results
-      # cjk1 (bigrams and unigrams):  3,087 results
-      # cjk3 (bigrams only):          3,087 results
-      # cjk7 (unigrams only):        27,879 results
-      # cjk6cn (chinese dict only):  4,980 results
-      # cjk6ja (japanese dict only): 19,599 results
+      # soc:                             24 results
+      # cjk1 (bigrams and unigrams):  3,087
+      # cjk3 (bigrams only):          3,087
+      # cjk7 (unigrams only):        27,879
+      # cjk6cn (chinese dict only):   4,980 
+      # cjk6ja (japanese dict only): 19,599 
+      # cjk8 (chinese dict no pos):   4,980
     end
     it "舊 小說 (space) should have good search results" do
       resp = solr_resp_doc_ids_only({'q'=>'舊 小說', 'rows'=>'20'}) 
       resp.should include(["9262744", "6797638", "6695967"]).as_first(3).results
       resp.should include(["6695904", "6696790"]).in_first(5).documents # both words in 245a
       resp.should include(["7198256", "6699444"]).in_first(8).documents  # both words in 245
-      # soc:  35 results
-      # cjk1 (bigrams and unigrams):  0 results 
-      # cjk3 (bigrams only):          0 results
-      # cjk7 (unigrams only):        73 results
-      # cjk6cn (chinese dict only):  10 results
-      # cjk6ja (japanese dict only): 68 results
+      # soc:                         35 results
+      # cjk1 (bigrams and unigrams):  0 
+      # cjk3 (bigrams only):          0 
+      # cjk7 (unigrams only):        73 
+      # cjk6cn (chinese dict only):  10 
+      # cjk6ja (japanese dict only): 68 
+      # cjk8 (chinese dict no pos):  10
     end
     context "title search" do
       it "舊小說 (no spaces) should have good search results" do
@@ -42,7 +44,7 @@ describe "Chinese: Word Breaks - Unigrams", :chinese => true, :fixme => true, :w
         resp.should include(["9262744", "6797638", "6695967"]).as_first(3).results
         resp.should include(["6695904", "6696790"]).in_first(5).documents # both words in 245a
         resp.should include(["7198256", "6699444"]).in_first(8).documents  # both words in 245
-        # soc:  11 results
+        # soc:                             11 results
         #   8834455  - ?? translated to simplified??
         #   7198256 - old 245b; fiction: 245a
         #   6793760 - ??
@@ -55,18 +57,19 @@ describe "Chinese: Word Breaks - Unigrams", :chinese => true, :fixme => true, :w
         #   6699444 - old 245a; fiction 245b
         #   6696790 - old 245a; fiction 245a
         # 
-        # cjk1 (bigrams and unigrams):  2,862 results  
+        # cjk1 (bigrams and unigrams):  2,862
         # cjk3 (bigrams only):          2,862
-        # cjk7 (unigrams only):        19,791 results
-        # cjk6cn (chinese dict only):   4,576 results
-        # cjk6ja (japanese dict only): 16,451 results
+        # cjk7 (unigrams only):        19,791
+        # cjk6cn (chinese dict only):   4,576 
+        # cjk6ja (japanese dict only): 16,451 
+        # cjk8 (chinese dict no pos):   4,576
       end  
       it "舊 小說 (space) should have good search results" do
         resp = solr_resp_doc_ids_only({'q'=>'舊 小說', 'rows'=>'20', 'qt'=>'search_title'}) 
         resp.should include(["9262744", "6797638", "6695967"]).as_first(3).results
         resp.should include(["6695904", "6696790"]).in_first(5).documents # both words in 245a
         resp.should include(["7198256", "6699444"]).in_first(8).documents  # both words in 245
-        # soc:  14 results
+        # soc:                        14 results
         #   8834455  - ?? translated to simplified??
         # NEW 7699186 - ??
         #   7198256 - old 245b; fiction: 245a
@@ -82,10 +85,10 @@ describe "Chinese: Word Breaks - Unigrams", :chinese => true, :fixme => true, :w
         # NEW 6698466 - old 245a; fiction 490a, 830a
         #   6696790 - old 245a; fiction 245a
         # 
-        # cjk1 (bigrams and unigrams):  0 results <--  what???
-        # cjk3 (bigrams only):          0 results
-        # cjk7 (unigrams only):        46 results
-        # cjk6cn (chinese dict only):   9 results
+        # cjk1 (bigrams and unigrams):  0 results
+        # cjk3 (bigrams only):          0 
+        # cjk7 (unigrams only):        46 
+        # cjk6cn (chinese dict only):   9 
         #    8834455
         #    9262744
         #    6797638
@@ -95,41 +98,150 @@ describe "Chinese: Word Breaks - Unigrams", :chinese => true, :fixme => true, :w
         #    6698466
         #    7198256
         #    6288832
-        # 
-        # cjk6ja (japanese dict only): 46 results
+        # cjk6ja (japanese dict only): 46
+        # cjk8 (chinese dict no pos):   9
       end
       
-    end
-    
-  end
+    end # title search    
+  end # old fiction
   
   context "婦女與文學 becomes  婦女 (women)  與 (and)   文學 (literature)" do
     #  婦女與文學    has 17 hits in soc;  soc would put it in the 3 units
     #  婦女與文學    parses to  婦女 (women)  與 (and)   文學 (literature) 
     #   女與   (BC chars)  has no meaning
     #   與文   (CD chars)  has no meaning on its own
+    # simplified:   妇女 与	文学
     
-    # no space
-    # space
-    # phrase no space
-    # phrase space
+    it "婦女與文學 (no spaces) should have good search results" do
+      resp = solr_resp_doc_ids_only({'q'=>'婦女與文學', 'rows'=>'20'}) 
+      resp.should include(["6343505", "8246653"]).as_first(2).documents
+      resp.should include(["4724601", "8250645", "6343719", "6343720"]).in_first(6).documents  # script as is
+      # soc:                              17 hits
+      # cjk1 (bigrams and unigrams):   7,201
+      # cjk3 (bigrams only):           7,201 
+      # cjk7 (unigrams only):        117,716
+      # cjk6cn (chinese dict only):   34,601
+      # cjk8 (chinese dict no pos):   34,601
+    end
+    it "婦女 與 文學 (spaces) should have good search results" do
+      resp = solr_resp_doc_ids_only({'q'=>'婦女 與 文學', 'rows'=>'20'}) 
+      # soc:                           21
+      # cjk1 (bigrams and unigrams):    0
+      # cjk3 (bigrams only):            0
+      # cjk7 (unigrams only):         107
+      # cjk6cn (chinese dict only):    15
+      # cjk8 (chinese dict no pos):    15
+    end
+    
+    context "title search" do
+      it "婦女與文學 (no spaces) should have good search results" do
+        resp = solr_resp_doc_ids_only({'q'=>'婦女與文學', 'rows'=>'20', 'qt'=>'search_title'}) 
+        # soc:                            11
+        #    8802530
+        #    8234101
+        #    7944106
+        #    7833961
+        #    8250645
+        #    8246653
+        #    5930857
+        #    4724601
+        #    6343719
+        #    6343505
+        #    6343720
+        # cjk1 (bigrams and unigrams):   6,723
+        # cjk3 (bigrams only):           6,723
+        # cjk7 (unigrams only):        102,211
+        # cjk6cn (chinese dict only):   32,505  
+        # cjk8 (chinese dict no pos):   32,505
+      end
+      it "婦女 與 文學 (spaces) should have good search results" do
+        resp = solr_resp_doc_ids_only({'q'=>'婦女 與 文學', 'rows'=>'20', 'qt'=>'search_title'}) 
+        # soc:                           11
+        #    8802530
+        #    8234101
+        #    7944106
+        #    7833961
+        #    8250645
+        #    8246653
+        #    5930857
+        #    4724601
+        #    6343719
+        #    6343505
+        #    6343720
+        # cjk1 (bigrams and unigrams):    0
+        # cjk3 (bigrams only):            0
+        # cjk7 (unigrams only):          61
+        # cjk6cn (chinese dict only):    13
+        # cjk8 (chinese dict no pos):    13
+      end
+  
+    end  # title search
+    
     # AND query
     # OR query
     # positionfilter ...
     # overlapping tokens ...   what if they didn't overlap and were ANDed
-  end
+  end # women and literature
 
   context " 三國誌 becomes  三國 (bigram) and  誌 (unigram)" do
-    # no space
-    # space
-    # phrase no space
-    # phrase space
+    it "三國誌 (no spaces) should have good search results" do
+      resp = solr_resp_doc_ids_only({'q'=>'三國誌', 'rows'=>'20'}) 
+      resp.should include(["6343505", "8246653"]).as_first(2).documents
+      resp.should include(["4724601", "8250645", "6343719", "6343720"]).in_first(6).documents  # script as is
+      # soc:                           242 
+      # cjk1 (bigrams and unigrams):   365
+      # cjk3 (bigrams only):           365
+      # cjk7 (unigrams only):       71,926
+      # cjk6cn (chinese dict only):    218
+      # cjk8 (chinese dict no pos):    218
+    end
+    it "三國 誌 (space) should have good search results" do
+      resp = solr_resp_doc_ids_only({'q'=>'三國 誌', 'rows'=>'20'}) 
+      # soc:                           247
+      # cjk1 (bigrams and unigrams):     0
+      # cjk3 (bigrams only):             0 
+      # cjk7 (unigrams only):          943
+      # cjk6cn (chinese dict only):  1,048   
+      # cjk8 (chinese dict no pos):  1,048
+    end
+    
+    context "title search" do
+      it "三國誌 (no spaces) should have good search results" do
+        resp = solr_resp_doc_ids_only({'q'=>'三國誌', 'rows'=>'20', 'qt'=>'search_title'}) 
+        # soc:                           162
+        # cjk1 (bigrams and unigrams):   315
+        # cjk3 (bigrams only):           315
+        # cjk7 (unigrams only):       54,939 
+        # cjk6cn (chinese dict only):    150
+        # cjk8 (chinese dict no pos):    150
+      end
+      it "三國 誌 (space) should have good search results" do
+        resp = solr_resp_doc_ids_only({'q'=>'三國 誌', 'rows'=>'20', 'qt'=>'search_title'}) 
+        # soc:                           162
+        # cjk1 (bigrams and unigrams):     0
+        # cjk3 (bigrams only):             0 
+        # cjk7 (unigrams only):          592   
+        # cjk6cn (chinese dict only):    407 
+        # cjk8 (chinese dict no pos):    407
+      end
+
     # AND query
     # OR query
     # positionfilter ...
     # overlapping tokens ...   what if they didn't overlap and were ANDed
+    end # title search
   end
   
-  
-  
+  context "飄" do
+    
+    it "does something" do
+      # best result:  6701323
+      # 飄  single char search
+      #   cjk6cn - chinese dict -->   69 results, but the best one is not at top
+      #   cjk6ja  69, but it is at the top   also  7737681
+      #   cjk8 - chinese dict but no position filter --  69 results, best at 2 top
+      pending "to be implemented"
+    end
+    
+  end
 end
