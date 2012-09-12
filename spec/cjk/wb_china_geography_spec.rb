7b305964d44ca2563999ebff475d03a6187b00dd
Naomi Dushay <ndushay@stanford.edu>
2012-09-12 14:48:29 -0700

    wrote real spec for wb_china_geography

--- spec/cjk/wb_china_geography_spec.rb
+++ spec/cjk/wb_china_geography_spec.rb
@@ -2,30 +2,48 @@
 require 'spec_helper'
 require 'rspec-solr'
 
-describe "Chinese: Word Breaks - a place in China geography", :chinese => true, :fixme => true, :wordbreak => true do
+describe "Chinese: Word Breaks", :chinese => true, :fixme => true, :wordbreak => true do
 
   context " 郑州 (a place in China)  地理 (geography)" do
-    it "郑州地理 should have good search results" do
-      resp = solr_resp_doc_ids_only({'q'=>'郑州地理', 'qt'=>'search_title'}) 
-      resp.should include("9612993").as_first.result  
-      resp.should have_at_most(1).document
-      # soc:                              2
-      #   9612993
-      #   5456407
-      # cjk1 (bigrams and unigrams):   2630 
-      # cjk3 (bigrams only):           2630 
-      # cjk7 (unigrams only):         39598
-      # cjk6cn (chinese dict only):    2235 
-      # cjk6ja (japanese dict only):   7668
-      # cjk8 (chinese dict no pos):    2235
+
+    shared_context "search tests" do
+      shared_examples "great search results for 郑州地理" do
+        describe do
+          it "should rank highest the documents with both words in 245a" do
+            resp.should include("9612993").as_first.result  
+          end
+          it "should rank high the documents with one word in 245a" do
+            #  地理 (geography) in 245a 490a, 830a;  郑州 (place in China) in 260a
+            resp.should include("4203541").in_first(3).results
+            #  地理 (geography) in 245a;  郑州 (place in China) in 260a
+            resp.should include(["4197620", "4808042", "5809974", "6704074"]).in_first(8).results
+          end
+          it "should rank less high the documents with both words present but not in 245" do
+            # 7833848	place 260a, geography 490a
+            resp.should include("7833848")
+            resp.should_not include("7833848").in_first(6).results
+            # 5456407	all four characters in 260
+            resp.should include("5456407")
+            resp.should_not include("5456407").in_first(6).results
+          end
+        end
+      end
+    end
+    include_context "search tests"
+
+    context "no spaces  郑州地理" do
+      # soc:                       2 : 9612993, 5456407
+      # cjk1 (bigrams only):    2630 
+      # cjk7 (unigrams only):  39598
+      # cjk6cn (chinese dict):  2235 
+      
+      it_behaves_like "great search results for 郑州地理" do
+        let (:resp) { solr_resp_doc_ids_only({'q'=>'郑州地理'}) }
+      end
     end
     
-    it "郑州 地理 as two terms should have good search results" do
-      resp = solr_resp_doc_ids_only({'q'=>'郑州 地理', 'qt'=>'search_title'})
-      resp.should include("9612993").as_first.result
-      resp.should have_at_most(1).document
-      
-      # soc:                           8
+    context "space between words 郑州 地理" do
+      # soc:                      8
       #     9612993
       #     7833848
       #     5809974
@@ -34,25 +52,36 @@ describe "Chinese: Word Breaks - a place in China geography", :chinese => true, 
       #     4197620
       #     5456407
       #     6704074
-      # cjk1 (bigrams and unigrams):   2
+      # cjk1 (bigrams only):      2
       #    4203541   place 260a; geography 245a, 490a, 830a
       #    5809974   place 260a; geography 245a
-      # cjk3 (bigrams only):           2 
-      # cjk7 (unigrams only):       1323
-      # cjk6cn (chinese dict only):    2   (same as cjk1)
-      # cjk6ja (japanese dict only):  81
-      # cjk8 (chinese dict no pos):    2
+      # cjk7 (unigrams only):  1323
+      # cjk6cn (chinese dict):    2   (same as cjk1)
+
+      it_behaves_like "great search results for 郑州地理" do
+        let (:resp) { solr_resp_doc_ids_only({'q'=>'郑州 地理'}) }
+      end
     end
-    
+          
     context "title search" do
-      it "郑州地理 should have good search results" do
-        resp = solr_resp_doc_ids_only({'q'=>'郑州地理', 'qt'=>'search_title'}) 
-        resp.should include("9612993").as_first.result  
-        resp.should have_at_most(1).document
-        # soc:                       1 result
+      
+      shared_context "title search tests" do
+        shared_examples "great title search results for 郑州地理" do
+          describe do
+            it "should only retrieve the documents with both words in 245a" do
+              resp.should include("9612993").as_first.result 
+              resp.should have_at_most(1).document 
+            end
+          end
+        end
+      end
+      include_context "title search tests"
+      
+      context "no spaces  郑州地理" do
+        # soc:                        1 result
         #    9612993     郑州地理 never;  郑州  245a, 520a, 651a;   地理  245a (but two chars between), 520a
         #  (not avail in dev)
-        # cjk1 (bigrams and unigrams): 2,286
+        # cjk1 (bigrams):         2,286
         #    6324746     郑州地理 never;  郑州 never;  地理 245a;   州地 (nonsense) 245a  (abcd - never, ab - never, cd - 245a; bc - 245a, bcd - 245a)
         #    5631095
         #    4188978
@@ -61,29 +90,30 @@ describe "Chinese: Word Breaks - a place in China geography", :chinese => true, 
         #    4822107
         #    4703364
         #    ...
-        # cjk3 (bigrams only):         2,286
-        # cjk7 (unigrams only):       31,543
-        # cjk6cn (chinese dict only):  2,016 
-        # cjk6ja (japanese dict only): 5,298
-        # cjk8 (chinese dict no pos):  2,016
+        # cjk7 (unigrams only):  31,543
+        # cjk6cn (chinese dict):  2,016 
+
+        it_behaves_like "great title search results for 郑州地理" do
+          let (:resp) { solr_resp_doc_ids_only({'q'=>'郑州地理', 'qt'=>'search_title'}) }
+        end
       end
       
-      it "郑州 地理 as two terms should have good search results" do
-        resp = solr_resp_doc_ids_only({'q'=>'郑州 地理', 'qt'=>'search_title'})
-        resp.should include("9612993").as_first.result
-        resp.should have_at_most(1).document
-        
+      context "space between words 郑州 地理" do
         # soc:  1 result
         #    9612993     郑州地理 never;  郑州  245a, 520a, 651a;   地理  245a (but two chars between), 520a
         #   (not avail in dev)
-        # cjk1 (bigrams and unigrams):   0
-        # cjk3 (bigrams only):           0 
+        # cjk1 (bigrams only):           0 
         # cjk7 (unigrams only):        746
         # cjk6cn (chinese dict only):    0
-        # cjk6ja (japanese dict only):  57 
         # cjk8 (chinese dict no pos):    0
-      end
+
+        it_behaves_like "great title search results for 郑州地理" do
+          let (:resp) { solr_resp_doc_ids_only({'q'=>'郑州 地理', 'qt'=>'search_title'}) }
+        end
+      end   
+         
     end # context title_search
+    
   end # context  (a place in China) (geography)
     
 end
