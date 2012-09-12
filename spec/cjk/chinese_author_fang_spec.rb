1e9965a0f8537b2fb62c8a9505f44ceeb902d970
Naomi Dushay <ndushay@stanford.edu>
2012-09-12 16:40:59 -0700

    more twiddling with fang author test

--- spec/cjk/chinese_author_fang_spec.rb
+++ spec/cjk/chinese_author_fang_spec.rb
@@ -5,16 +5,7 @@ require 'rspec-solr'
 describe "Chinese Author Search (Fang, Baochuan)", :chinese => true, :fixme => true do
 
   shared_context "search tests" do
-    # 7 in soc
-    #   9388854 - in 700a, 245c  trad
-    #   6650839 - in 700a, 245c  trad
-    #   5727562 - in 700a, 245c  trad
-    #   5482057 - in 700a, 245c  trad
-    #   8243512 - <-- Why does this match??
-    #   5468624 - in 700a, 245c  trad
-    #   4160175 - in 700a, 245c  simplified
-    
-    shared_examples "great author search results" do
+    shared_examples "great results" do
       describe do
         it "should get all the  方寶川 (traditional char) matches" do
           resp.should include(["5468624", "5482057", "5727562", "6650839", "9388854"]).in_first(10).results
@@ -25,19 +16,41 @@ describe "Chinese Author Search (Fang, Baochuan)", :chinese => true, :fixme => t
         it "should get an appropriate number of documents" do
           resp.should have_at_most(15).documents
         end
+        # 7 in soc
+        #   9388854 - in 700a, 245c  trad
+        #   6650839 - in 700a, 245c  trad
+        #   5727562 - in 700a, 245c  trad
+        #   5482057 - in 700a, 245c  trad
+        #   8243512 - <-- Why does this match??
+        #   5468624 - in 700a, 245c  trad
+        #   4160175 - in 700a, 245c  simplified
+        # 
+        # cjk9a matches simplified and traditional separately
+        #  AND
+        # gets these for both
+        # 1467632
+    		# 1121907
+    		# 1131798
+    		# 6626069
+    		# 6274452
+    		# 1144876
+    		# 146147
+    		# 1130079
+    		# 240893
+    		# 579784
       end
     end
   end
   include_context "search tests"
   
   context "traditional  方寶川" do
-    it_behaves_like "great author search results" do
+    it_behaves_like "great results" do
       let (:resp) { solr_resp_doc_ids_only({'q'=>'方寶川', 'qt'=>'search_author'}) }
     end
   end
   
   context "simplified  方宝川" do
-    it_behaves_like "great author search results" do
+    it_behaves_like "great results" do
       let (:resp) { solr_resp_doc_ids_only({'q'=>'方宝川', 'qt'=>'search_author'}) }
     end
   end   
