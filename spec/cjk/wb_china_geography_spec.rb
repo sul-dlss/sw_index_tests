0f341024cfe6f40352014224e97ac9c40e4fa3e3
Naomi Dushay <ndushay@stanford.edu>
2012-09-12 14:57:08 -0700

    big revision to china_geography test -- only use title search or too many results

--- spec/cjk/wb_china_geography_spec.rb
+++ spec/cjk/wb_china_geography_spec.rb
@@ -6,12 +6,43 @@ describe "Chinese: Word Breaks", :chinese => true, :fixme => true, :wordbreak =>
 
   context " 郑州 (a place in China)  地理 (geography)" do
 
+    context "title search" do
+      
+      shared_context "title search tests" do
+        shared_examples "great title search results for 郑州地理" do
+          describe do
+            it "should only retrieve the documents with both words in 245a" do
+              resp.should include("9612993").as_first.result 
+              resp.should have_at_most(1).document 
+            end
+          end
+        end
+      end
+      include_context "title search tests"
+      
+      context "no spaces  郑州地理" do
+        it_behaves_like "great title search results for 郑州地理" do
+          let (:resp) { solr_resp_doc_ids_only({'q'=>'郑州地理', 'qt'=>'search_title'}) }
+        end
+      end
+      
+      context "space between words 郑州 地理" do
+        it_behaves_like "great title search results for 郑州地理" do
+          let (:resp) { solr_resp_doc_ids_only({'q'=>'郑州 地理', 'qt'=>'search_title'}) }
+        end
+      end   
+         
+    end # context title_search
+
+=begin
     shared_context "search tests" do
       shared_examples "great search results for 郑州地理" do
         describe do
           it "should rank highest the documents with both words in 245a" do
             resp.should include("9612993").as_first.result  
           end
+# FIXME?          
+          # there are a LOT more of these --> socrates finds 1953 of them.
           it "should rank high the documents with one word in 245a" do
             #  地理 (geography) in 245a 490a, 830a;  郑州 (place in China) in 260a
             resp.should include("4203541").in_first(3).results
@@ -63,57 +94,7 @@ describe "Chinese: Word Breaks", :chinese => true, :fixme => true, :wordbreak =>
       end
     end
           
-    context "title search" do
-      
-      shared_context "title search tests" do
-        shared_examples "great title search results for 郑州地理" do
-          describe do
-            it "should only retrieve the documents with both words in 245a" do
-              resp.should include("9612993").as_first.result 
-              resp.should have_at_most(1).document 
-            end
-          end
-        end
-      end
-      include_context "title search tests"
-      
-      context "no spaces  郑州地理" do
-        # soc:                        1 result
-        #    9612993     郑州地理 never;  郑州  245a, 520a, 651a;   地理  245a (but two chars between), 520a
-        #  (not avail in dev)
-        # cjk1 (bigrams):         2,286
-        #    6324746     郑州地理 never;  郑州 never;  地理 245a;   州地 (nonsense) 245a  (abcd - never, ab - never, cd - 245a; bc - 245a, bcd - 245a)
-        #    5631095
-        #    4188978
-        #    6827354
-        #    8720366
-        #    4822107
-        #    4703364
-        #    ...
-        # cjk7 (unigrams only):  31,543
-        # cjk6cn (chinese dict):  2,016 
-
-        it_behaves_like "great title search results for 郑州地理" do
-          let (:resp) { solr_resp_doc_ids_only({'q'=>'郑州地理', 'qt'=>'search_title'}) }
-        end
-      end
-      
-      context "space between words 郑州 地理" do
-        # soc:  1 result
-        #    9612993     郑州地理 never;  郑州  245a, 520a, 651a;   地理  245a (but two chars between), 520a
-        #   (not avail in dev)
-        # cjk1 (bigrams only):           0 
-        # cjk7 (unigrams only):        746
-        # cjk6cn (chinese dict only):    0
-        # cjk8 (chinese dict no pos):    0
-
-        it_behaves_like "great title search results for 郑州地理" do
-          let (:resp) { solr_resp_doc_ids_only({'q'=>'郑州 地理', 'qt'=>'search_title'}) }
-        end
-      end   
-         
-    end # context title_search
-    
+=end    
   end # context  (a place in China) (geography)
     
 end
