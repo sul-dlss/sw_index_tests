7141d0d572854997f172d9d23b820502e15ae2bd
Naomi Dushay <ndushay@stanford.edu>
2012-10-12 11:33:21 -0700

    add parens_spec

---
+++ spec/parens_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require 'rspec-solr'
+
+describe "Searches With Parens" do
+  
+  it "q of 'french beans food scares' has specific match and non-match" do
+    resp = solr_resp_doc_ids_only({'q'=>'french beans food scares'})
+    resp.should include("7716344").as_first_result
+    resp.should_not include("69555562")
+  end
+  
+end
