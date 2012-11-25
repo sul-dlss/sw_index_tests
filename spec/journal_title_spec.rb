021aabdefdceb0ff49cec2686942be36f1614a9e
Naomi Dushay <ndushay@stanford.edu>
2012-11-24 23:56:17 -0800

    refinements to journal title spec

--- spec/journal_title_spec.rb
+++ spec/journal_title_spec.rb
@@ -1,14 +1,35 @@
 require 'spec_helper'
 
 describe "journal titles" do
-  it "nature (Natural stems to Nation)" do
+
+  it "nature as everything search", :jira => 'VUF-1515' do
+    resp = solr_response({'q' => 'nature'}.merge({'fl'=>'id,title_display', 'facet'=>false}))
+    resp.should include({'title_display' => /^Nature \[print\/digital\]\./}).in_first(3)
+  end
+
+  it "nature as title search" do
+    resp = solr_response(title_search_args('nature').merge({'fl'=>'id,title_display', 'facet'=>false}))
+    resp.should include({'title_display' => /^Nature \[print\/digital\]\./}).in_first(3)
+  end
+
+  it "nature (Natural stems to Nation) with format journal" do
     resp = solr_response(title_search_args('nature').merge({'fq' => 'format:"Journal/Periodical"', 'fl'=>'id,title_display', 'facet'=>false}))
     resp.should have_at_most(1100).documents
     resp.should include({'title_display' => /^Nature \[print\/digital\]\./}).in_first(5)
     resp.should include({'title_display' => /^Nature; international journal of science/}).in_first(5)
   end
   
-  it "The Nation (National stems to Nation)" do
+  it "The Nation as everything search" do
+    resp = solr_resp_doc_ids_only({'q' => 'The Nation'})
+    resp.should include(['464445', '497417', '3448713']).in_first(4)
+  end
+
+  it "The Nation as title search", :fixme => true do
+    resp = solr_resp_doc_ids_only(title_search_args('The Nation'))
+    resp.should include(['464445', '497417', '3448713']).in_first(4)
+  end
+
+  it "The Nation (National stems to Nation) with format journal" do
     resp = solr_resp_doc_ids_only(title_search_args('The Nation').merge({'fq' => 'format:Journal/Periodical'}))
     resp.should include('3448713').in_first(3)
     resp.should include(['3448713', '497417']).in_first(5)
