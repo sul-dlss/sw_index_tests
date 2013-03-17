b8e3b818bd90e587b9cbf1e55c17d98700af78f2
Naomi Dushay <ndushay@stanford.edu>
2013-03-17 15:20:12 -0700

    add solr_resp_ids_from_query method to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,10 +16,17 @@ RSpec.configure do |config|
   puts "Solr URL: #{@@solr.uri}"
 end
 
+# send a GET request to the default Solr request handler with the indicated query
+# @param query [String] the value of the 'q' param to be sent to Solr
+# @return [RSpecSolr::SolrResponseHash] object for rspec-solr testing the Solr response, with no facets, and only the id field for each Solr doc
+def solr_resp_ids_from_query(query)
+  resp = solr_resp_doc_ids_only({'q'=> query})
+end
+
 # send a GET request to the default Solr request handler with the indicated Solr parameters
 # @param solr_params [Hash] the key/value pairs to be sent to Solr as HTTP parameters, in addition to 
 #  those to get only id fields and no facets in the response
-# @return [RSpecSolr::SolrResponseHash] object for rspec-solr testing the Solr response 
+# @return [RSpecSolr::SolrResponseHash] object for rspec-solr testing the Solr response, with no facets, and only the id field for each Solr doc
 def solr_resp_doc_ids_only(solr_params)
   solr_response(solr_params.merge(@@doc_ids_only))
 end
