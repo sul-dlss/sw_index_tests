f63e51ec301f8aa836eb96c257d064e145f538ed
Naomi Dushay <ndushay@stanford.edu>
2013-04-15 17:39:18 -0700

    change image gallery specs to gdor specs

--- spec/full_index_contents_spec.rb
+++ spec/full_index_contents_spec.rb
@@ -4,30 +4,30 @@ describe "Index Contents" do
 
   it "collection filter query for sirsi should contain all the MARC data from SIRSI (Symphony)" do
     resp = solr_resp_doc_ids_only({'fq'=>'collection:sirsi', 'rows'=>'0'})
-    resp.should have_at_least(6850000).documents
+    resp.should have_at_least(6900000).documents
   end
   
-  context "Image Gallery Collections" do
+  context "DOR Digital Collections" do
     context "Kolb" do
       it "collection filter query should have at least 1150 total results" do
-        resp = solr_resp_doc_ids_only({'fq'=>'collection:"Leon Kolb Collection of Portraits"', 'rows'=>'0'})
+        resp = solr_resp_doc_ids_only({'fq'=>'collection:4084372', 'rows'=>'0'})
         resp.should have_at_least(1150).documents
       end
       it "item object should be retrieved via everything search" do
-        resp = solr_resp_doc_ids_only({'q'=>'Bourbon Antoine'})
-        resp.should include('ikolb-0147').in_first(10)
+        resp = solr_resp_doc_ids_only({'q'=>'Addison Joseph'})
+        resp.should include('vb267mw8946').in_first(10)
         # And I should see a portrait of a guy with big hair
       end      
     end
 
     context "Reid Dennis" do
-      it "collection filter query should have at least 1150 total results" do
-        resp = solr_resp_doc_ids_only({'fq'=>'collection:"The Reid W. Dennis Collection of California Lithographs"', 'rows'=>'0'})
+      it "collection filter query should have at least 46 total results" do
+        resp = solr_resp_doc_ids_only({'fq'=>'collection:6780453', 'rows'=>'0'})
         resp.should have_at_least(46).documents
       end
       it "item object should be retrieved via everything search" do
         resp = solr_resp_doc_ids_only({'q'=>'birds-eye view san francisco'})
-        resp.should include(['ird-250', 'ird-122', 'ird-316']).in_first(10)
+        resp.should include(['pz572zt9333', 'nz525ps5073', 'bw260mc4853', 'mz639xs9677']).in_first(15)
       end       
     end
   end
