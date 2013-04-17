075ae61bb25a110d2a647c316c2fe5c5e3f1ac3b
Naomi Dushay <ndushay@stanford.edu>
2013-04-17 14:48:58 -0700

    add tests for merged collection records for Kolb and Reid-Dennis

--- spec/full_index_contents_spec.rb
+++ spec/full_index_contents_spec.rb
@@ -17,7 +17,18 @@ describe "Index Contents" do
         resp = solr_resp_doc_ids_only({'q'=>'Addison Joseph'})
         resp.should include('vb267mw8946').in_first(10)
         # And I should see a portrait of a guy with big hair
-      end      
+      end
+      context "merged collection object" do
+        before(:all) do
+          @resp = solr_response({'qt'=>'document', 'id'=>'4084372', 'fl'=>'id,url_fulltext,collection_type', 'facet'=>false})
+        end
+        it "collection object should have purl url in url_fulltext" do
+          @resp.should include("url_fulltext" => 'http://purl.stanford.edu/bs646cd8717')
+        end
+        it "collection object should have collection_type field" do
+          @resp.should include("collection_type" => 'Digital Collection')
+        end
+      end
     end
 
     context "Reid Dennis" do
@@ -28,7 +39,18 @@ describe "Index Contents" do
       it "item object should be retrieved via everything search" do
         resp = solr_resp_doc_ids_only({'q'=>'birds-eye view san francisco'})
         resp.should include(['pz572zt9333', 'nz525ps5073', 'bw260mc4853', 'mz639xs9677']).in_first(15)
-      end       
+      end
+      context "merged collection object" do
+        before(:all) do
+          @resp = solr_response({'qt'=>'document', 'id'=>'6780453', 'fl'=>'id,url_fulltext,collection_type', 'facet'=>false})
+        end
+        it "collection object should have purl url in url_fulltext" do
+          @resp.should include("url_fulltext" => 'http://purl.stanford.edu/sg213ph2100')
+        end
+        it "collection object should have collection_type field" do
+          @resp.should include("collection_type" => 'Digital Collection')
+        end
+      end
     end
   end
   
