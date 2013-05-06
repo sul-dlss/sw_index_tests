03dada7f92cac45edb79a14fddcdf7dfe6963b6f
Naomi Dushay <ndushay@stanford.edu>
2013-05-06 15:48:34 -0700

    update non-alpha-char spec for Two3 search

--- spec/non_alpha_chars_spec.rb
+++ spec/non_alpha_chars_spec.rb
@@ -6,7 +6,7 @@ describe "Terms with Numbers or other oddities" do
     resp = solr_resp_doc_ids_only({'q'=>'Two3'})
     resp.should have_at_most(10).documents
     resp.should include("5732752").as_first_result
-    resp.should include("8564713").in_first(2).results
+    resp.should include("5732855")
     resp.should_not include("5727394")
     resp.should have_the_same_number_of_results_as(solr_resp_doc_ids_only({'q'=>'two3'}))
     resp.should have_fewer_results_than(solr_resp_doc_ids_only({'q'=>'two 3'}))
