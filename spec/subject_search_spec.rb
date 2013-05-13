42c6123e502bd3a85553b6eb38dc0d8341cb325b
Naomi Dushay <ndushay@stanford.edu>
2013-05-13 14:44:12 -0700

    remove obsolete subject spec for c programming

--- spec/subject_search_spec.rb
+++ spec/subject_search_spec.rb
@@ -23,14 +23,6 @@ describe "Subject Search" do
     resp.should_not include("8545853")  # has neither
   end
 
-# NOTE:  this test is superceded by test in synonym_spec as of 2013-05-10
-  it "C programming", :jira => 'VUF-1993', :fixme => true do
-    resp = solr_resp_doc_ids_only(subject_search_args('C programming'))
-    resp.should have_at_least(1325).results
-    resp.should have_at_most(1525).results
-    resp.should include("4617632")
-  end
-  
   it "china women history", :jira => 'VUF-873' do
     resp = solr_resp_doc_ids_only(subject_search_args('China women history').merge({:fq => 'language:English'}))
     resp.should have_at_least(170).results
