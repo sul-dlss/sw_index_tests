e7c842ec946c972e25f784f5f81a58fe8267b124
Naomi Dushay <ndushay@stanford.edu>
2013-05-14 17:11:13 -0700

    added possible future sorting test notes

--- spec/sort_spec.rb
+++ spec/sort_spec.rb
@@ -25,4 +25,42 @@ describe "sorting results" do
     end    
   end # empty query
 
+=begin
+  Scenario: Spaces should be significant
+  Scenario: Case / Capitalization should have no effect on sorting
+  Scenario: Punctuation should not affect sorting
+  Scenario: Subscripts should be sorted properly
+  # super scripts;  what about $, %, etc.
+
+  Scenario: Letters with and without diacritics should be interfiled
+    # TODO:  diacritics in first character;  subsequent characters  
+  Scenario: Polish L should sort properly
+  Scenario: Znaks, hard and soft, should be ignored for sorting
+    # More information needed about znaks: is this a character?  a diacritic?  Should any occurrence be ignored?
+
+  Scenario: Æ and AE should be interfiled
+    And I go to the home page
+    When I fill in "q" with "Æon"
+    And I press "search"
+    And I select "title" from "sort"
+    And I press "sort_submit"
+    Then I should get ckey 6628532 before ckey 4647437
+    Then I should get ckey 6197318 before ckey 4647437
+
+  Scenario: Chinese - traditional and simplified characters should be sorted together
+  Scenario: Japanese - old and new characters should sort together?
+  Scenario: Korean - something about spaces vs. no space (?)
+  Scenario: Hebrew alif and ayn should be ignored for sorting
+    # TODO:  as first character only, or as any character?
+    # TODO:  transliteration vs. hebrew script ... 
+
+
+  Scenario: Combination of non-filing characters and diacritics in first character should sort properly.
+     # Etude,   another example from vitus in email week of 4/27
+
+  Scenario: Non-filing indicators should be ignored for sorting.
+   # TODO: maybe someday autodetect non-filing chars that aren't accommodated in the marc record
+
+=end
+
 end
