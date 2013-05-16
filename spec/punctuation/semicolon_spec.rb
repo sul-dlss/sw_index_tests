000b2855723e79c27cced5086c5a05153eea8ceb
Naomi Dushay <ndushay@stanford.edu>
2013-05-16 15:11:54 -0700

    make semicolon spec implementation cleaner

--- spec/punctuation/semicolon_spec.rb
+++ spec/punctuation/semicolon_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "semicolon in queries" do
   it "at end of query preceded by a space should be ignored" do
-    resp = solr_resp_doc_ids_only({'q'=>'Lecture notes in statistics ;'})
-    resp.should have_the_same_number_of_results_as(solr_resp_doc_ids_only({'q'=>'Lecture notes in statistics'}))
+    resp = solr_resp_ids_from_query 'Lecture notes in statistics ;'
+    resp.should have_the_same_number_of_results_as(solr_resp_ids_from_query 'Lecture notes in statistics')
   end
 end
