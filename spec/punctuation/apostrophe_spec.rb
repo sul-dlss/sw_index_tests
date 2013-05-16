b282fcac272d1aaa1312e74c5b5cca96b5ab1b98
Naomi Dushay <ndushay@stanford.edu>
2013-05-16 15:10:09 -0700

    clean up apostrophe spec a bit

--- spec/punctuation/apostrophe_spec.rb
+++ spec/punctuation/apostrophe_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 
 describe "Apostrophes", :jira => ['SW-648', 'SW-754'], :fixme => true do
   it "m'arthur" do
-    resp = solr_resp_doc_ids_only({'q'=>"m'arthur"})
-    resp.should have_fewer_results_than(solr_resp_doc_ids_only({'q'=>"marthur"}))
-    # to be implemented
-#    resp.should_not have_same_first_result_as(solr_resp_doc_ids_only({'q'=>"m arthur"}))
+    resp = solr_resp_ids_from_query "m'arthur"
+    resp.should have_fewer_results_than(solr_resp_ids_from_query "marthur")
     resp.should_not include("2402216").in_first(12).documents
+    pending "need to implement have_same_first_result_as"
+#    resp.should_not have_same_first_result_as(solr_resp_ids_from_query "m arthur" )
   end
   
 end
