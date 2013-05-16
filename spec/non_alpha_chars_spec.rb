66a0596cf915f2239d654f26a2a5ecadfa98175d
Naomi Dushay <ndushay@stanford.edu>
2013-05-15 17:06:51 -0700

    fix typo in non_alpha_chars_spec  ignore punctuation inside a phrase

--- spec/non_alpha_chars_spec.rb
+++ spec/non_alpha_chars_spec.rb
@@ -89,7 +89,7 @@ describe "Terms with Numbers or other oddities" do
   end # context slash
   
   it "should ignore punctuation inside a phrase" do
-    resp = solr_resp_doc_ids_only({'q'=>'""Alice in Wonderland : a serie[s]"'})
+    resp = solr_resp_doc_ids_only({'q'=>'"Alice in Wonderland : a serie[s]"'})
     resp.should have_at_least(2).documents
     resp.should have_the_same_number_of_results_as(solr_resp_doc_ids_only({'q'=>'"Alice in Wonderland a serie[s]"'}))
   end
