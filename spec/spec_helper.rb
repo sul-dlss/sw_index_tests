068110a1bf9bcc4fd99de4c236f6d507c54b50ab
Naomi Dushay <ndushay@stanford.edu>
2013-06-06 18:40:50 -0700

    spec_helper now has doc_ids_short_titles, and uses pf2 and pf3 args for edismax which will be ignored for dismax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,26 +28,34 @@ end
 #  those to get only id fields and no facets in the response
 # @return [RSpecSolr::SolrResponseHash] object for rspec-solr testing the Solr response, with no facets, and only the id field for each Solr doc
 def solr_resp_doc_ids_only(solr_params)
-  solr_response(solr_params.merge(@@doc_ids_only))
+  solr_response(solr_params.merge(doc_ids_only))
+end
+
+# send a GET request to the default Solr request handler with the indicated Solr parameters
+# @param solr_params [Hash] the key/value pairs to be sent to Solr as HTTP parameters, in addition to 
+#  those to get only id fields and title_245a_display and no facets in the response
+# @return [RSpecSolr::SolrResponseHash] object for rspec-solr testing the Solr response, with no facets, and only the id and short title field for each Solr doc
+def solr_resp_ids_titles(solr_params)
+  solr_response(solr_params.merge(doc_ids_short_titles))
 end
 
 def author_search_args(query_str)
-  {'q'=>"{!qf=$qf_author pf=$pf_author}#{query_str}", 'qt'=>'search'}
+  {'q'=>"{!qf=$qf_author pf=$pf_author pf2=$pf_author pf3=$pf_author}#{query_str}", 'qt'=>'search'}
 end
 def callnum_search_args(query_str)
   {'q'=>"#{query_str}", 'defType'=>'lucene', 'df'=>'callnum_search', 'qt'=>'search'}
 end
 def subject_search_args(query_str)
-  {'q'=>"{!qf=$qf_subject pf=$pf_subject}#{query_str}", 'qt'=>'search'}
+  {'q'=>"{!qf=$qf_subject pf=$pf_subject pf2=$pf_subject pf3=$pf_subject}#{query_str}", 'qt'=>'search'}
 end
 def series_search_args(query_str)
-  {'q'=>"{!qf=$qf_series pf=$pf_series}#{query_str}", 'qt'=>'search'}
+  {'q'=>"{!qf=$qf_series pf=$pf_series pf2=$pf_series pf3=$pf_series}#{query_str}", 'qt'=>'search'}
 end
 def title_search_args(query_str)
-  {'q'=>"{!qf=$qf_title pf=$pf_title}#{query_str}", 'qt'=>'search'}
+  {'q'=>"{!qf=$qf_title pf=$pf_title pf2=$pf_title pf3=$pf_title}#{query_str}", 'qt'=>'search'}
 end
 def author_title_search_args(query_str)
-  {'q'=>"{!qf=author_title_search pf='author_title_search^10'}\"#{query_str}\"", 'qt'=>'search'}
+  {'q'=>"{!qf=author_title_search pf='author_title_search^10' pf2='author_title_search^20' pf3='author_title_search^30'}\"#{query_str}\"", 'qt'=>'search'}
 end
 
 private
@@ -63,12 +71,18 @@ end
 # use these Solr HTTP params to reduce the size of the Solr responses
 # response documents will only have id fields, and there will be no facets in the response
 @@doc_ids_only = {'fl'=>'id', 'facet'=>'false'}
+@@doc_ids_short_titles = {'fl'=>'id,title_245a_display', 'facet'=>'false'}
 
-# use these Solr HTTP params to reduce the size of the Solr responses
 # response documents will only have id fields, and there will be no facets in the response
 # @return [Hash] Solr HTTP params to reduce the size of the Solr responses
 def doc_ids_only
   @@doc_ids_only
+end
+
+# response documents will only have id and title_245a_display fields, and there will be no facets in the response
+# @return [Hash] Solr HTTP params to reduce the size of the Solr responses
+def doc_ids_short_titles
+  @@doc_ids_short_titles
 end
 
 def solr_schema
