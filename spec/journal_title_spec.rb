989e8ff56fbbbb061ca109d42d38d2b0a8005193
Naomi Dushay <ndushay@stanford.edu>
2013-06-10 14:43:42 -0700

    add the Guardian to journal title specs;  refactor shared example groups for more flexibility

--- spec/journal_title_spec.rb
+++ spec/journal_title_spec.rb
@@ -56,52 +56,148 @@ describe "journal/newspaper titles" do
     it "exact title matches should be first" do
       orig_query_str = solr_params['q'].split('}').last
       resp = solr_resp_ids_titles(solr_params)
-      resp.should include({'title_245a_display' => /^#{orig_query_str}$/i}).in_each_of_first(exp_ids.size)
+      resp.should include({'title_245a_display' => /^#{orig_query_str}\W*$/i}).in_each_of_first(exp_ids.size)
       resp.should include(exp_ids).in_first(exp_ids.size + 2) # a little slop built in
     end
   end   
 
-  shared_examples_for 'everything query, no format specified' do | title |
-    it_behaves_like "great search results", {'q'=>title} 
+  shared_examples_for 'everything query, no format specified' do | title, solr_params |
+    my_params = {'q'=>title}
+    my_params.merge!(solr_params) if solr_params
+    it_behaves_like "great search results", my_params 
   end   
-  shared_examples_for 'everything query, format journal' do | title |
-    it_behaves_like "great search results", {'q'=>title, 'fq'=>'format:Journal/Periodical'} 
+  shared_examples_for 'everything query, format journal' do | title, solr_params |
+    my_params = {'q'=>title, 'fq'=>'format:Journal/Periodical'}
+    my_params.merge!(solr_params) if solr_params
+    it_behaves_like "great search results", my_params
   end
-  shared_examples_for 'everything query, format newspaper' do | title |
-    it_behaves_like "great search results", {'q'=>title, 'fq'=>'format:Newspaper'} 
+  shared_examples_for 'everything query, format newspaper' do | title, solr_params |
+    my_params = {'q'=>title, 'fq'=>'format:Newspaper'}
+    my_params.merge!(solr_params) if solr_params
+    it_behaves_like "great search results", my_params
   end
 
-  shared_examples_for 'title query, no format specified' do | title |
-    it_behaves_like "great search results", title_search_args(title) 
+  shared_examples_for 'title query, no format specified' do | title, solr_params |
+    my_params = title_search_args(title) 
+    my_params.merge!(solr_params) if solr_params
+    it_behaves_like "great search results", my_params
   end   
-  shared_examples_for 'title query, format journal' do | title |
-    it_behaves_like "great search results", title_search_args(title).merge({'fq'=>'format:Journal/Periodical'}) 
+  shared_examples_for 'title query, format journal' do | title, solr_params |
+    my_params = title_search_args(title).merge({'fq'=>'format:Journal/Periodical'})
+    my_params.merge!(solr_params) if solr_params
+    it_behaves_like "great search results", my_params
   end
-  shared_examples_for 'title query, format newspaper' do | title |
-    it_behaves_like "great search results", title_search_args(title).merge({'fq'=>'format:Newspaper'})
+  shared_examples_for 'title query, format newspaper' do | title, solr_params |
+    my_params = title_search_args(title).merge({'fq'=>'format:Newspaper'})
+    my_params.merge!(solr_params) if solr_params
+    it_behaves_like "great search results", my_params
   end
   
-  shared_examples_for 'great results for journal/newspaper' do | title|
-    it_behaves_like "everything query, no format specified", title do
-      let(:exp_ids) {all_formats}
-    end
-    it_behaves_like "everything query, format journal", title do
+  shared_examples_for 'great results for format journal' do | title, solr_params |
+    it_behaves_like "everything query, format journal", title, solr_params do
       let(:exp_ids) {journal_only}
     end
-    it_behaves_like "everything query, format newspaper", title do
+    it_behaves_like "title query, format journal", title, solr_params do
+      let(:exp_ids) {journal_only}
+    end
+  end
+  
+  shared_examples_for 'great results for format newspaper' do | title, solr_params |
+    it_behaves_like "everything query, format newspaper", title, solr_params do
       let(:exp_ids) {newspaper_only}
     end
-    it_behaves_like "title query, no format specified", title do
+    it_behaves_like "title query, format newspaper", title, solr_params do
+      let(:exp_ids) {newspaper_only}
+    end
+  end
+  
+  
+  shared_examples_for 'great results for journal/newspaper' do | title, solr_params |
+    it_behaves_like "everything query, no format specified", title, solr_params do
       let(:exp_ids) {all_formats}
     end
-    it_behaves_like "title query, format journal", title do
+    it_behaves_like "title query, no format specified", title, solr_params do
+      let(:exp_ids) {all_formats}
+    end
+    it_behaves_like "great results for format journal", title, solr_params do
       let(:exp_ids) {journal_only}
     end
-    it_behaves_like "title query, format newspaper", title do
+    it_behaves_like "great results for format newspaper", title, solr_params do
       let(:exp_ids) {newspaper_only}
     end
   end
 
+  shared_examples_for 'good results for journal/newspaper' do | title, solr_params |
+    it_behaves_like "great results for format journal", title, solr_params do
+      let(:exp_ids) {journal_only}
+    end
+    it_behaves_like "great results for format newspaper", title, solr_params do
+      let(:exp_ids) {newspaper_only}
+    end
+  end
+
+  context "The Guardian" do
+    it_behaves_like "great results for format newspaper", "The Guardian" do
+      green_mfilm2 = '491941'
+      green_manchester = '438344'
+      nigeria = '2873190'
+      tanzania = '4720924'
+      hoover_manchester = '411072'
+      let(:newspaper_only) { [green_mfilm2, green_manchester, nigeria, tanzania, hoover_manchester] }
+    end
+    context "fixme", :fixme => true do
+      it_behaves_like "great results for format journal", "The Guardian" do
+        # problematic:
+        #   '6541023'  #  245  6| 880-01 a| Dao bao. b| The Guardian.
+        rare = '473061'
+        rare2 = '2046773'
+        rare3 = '2046781'
+        green_mfilm = '361891'
+        green_philly = '361893'
+        let(:journal_only) { [rare, rare2, rare3, green_mfilm, green_philly]  }
+      end
+
+      it_behaves_like "great results for journal/newspaper", "The Guardian", {'rows' => 64 } do
+        rare = '473061'
+        rare2 = '2046773'
+        rare3 = '2046781'
+        green_mfilm = '361891'
+        green_philly = '361893'
+        journal = [rare, rare2, rare3, green_mfilm, green_philly]      
+
+        green_mfilm2 = '491941'
+        green_manchester = '438344'
+        nigeria = '2873190'
+        tanzania = '4720924'
+        hoover_manchester = '411072'
+        news = [green_mfilm2, green_manchester, nigeria, tanzania, hoover_manchester]
+
+        video = ['6739108']
+
+        burma = '382502' # format Other
+        hoover = '382501' # format Other
+        other = [burma, hoover]
+
+        # many of the book are from find.galegroup.com
+        book = ['8415217', '8414328', '8402896', '8738551', '8744676', '8744669', '8318844', '2075676', '2075676', '3042946', '2074558', '2073994',
+          '2817859', '8318845', '8054820', '8054819', '50179', '596533', '6982889', '8085274', '8737844', 
+          '8061605', '8061610', '8061608', '8061609', '8033895', '8061606', '8061607', '8733947', '8009153', '8054821']
+        # all of the below are online from find.galegroup.com
+        ellipses = ['8074878', '8057381', '8076326', '8002481', '8029616', '8022667', '8002480', '8026648', '8026647', '8080945', '8002479', 
+                    '8002478', '8026645', '8054818', '8057382', '8026646', '8076327', '8026644', '8078462', '' ]
+
+        # problematic:
+        #   '6541023'  #  245  6| 880-01 a| Dao bao. b| The Guardian.
+        #   '8161510'  #  245  a| The Guardian (Charlottetown)
+
+        all = journal + news + video + other + book + ellipses
+        let(:all_formats) { all }
+        let(:journal_only) { journal }
+        let(:newspaper_only) { news }
+      end
+    end
+  end # the Guardian
+  
   context "The Sentinel" do
     it_behaves_like "great results for journal/newspaper", "The Sentinel" do
       online = "8169876"  # 2044-6071, marcit brief record, type 'Other' as of 2013-06-06
