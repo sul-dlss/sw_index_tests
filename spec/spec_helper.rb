52460ad8079f10651e441388f00a6419b00d4560
Naomi Dushay <ndushay@stanford.edu>
2013-06-27 16:56:19 -0700

    add methods to spec_helper to get short and full titles easily

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,20 @@ def solr_resp_ids_from_query(query)
   resp = solr_resp_doc_ids_only({'q'=> query})
 end
 
+# send a GET request to the default Solr request handler with the indicated query
+# @param query [String] the value of the 'q' param to be sent to Solr
+# @return [RSpecSolr::SolrResponseHash] object for rspec-solr testing the Solr response, with no facets, and only the id and title_245a_display field for each Solr doc
+def solr_resp_ids_titles_from_query(query)
+  resp = solr_resp_ids_titles({'q'=> query})
+end
+
+# send a GET request to the default Solr request handler with the indicated query
+# @param query [String] the value of the 'q' param to be sent to Solr
+# @return [RSpecSolr::SolrResponseHash] object for rspec-solr testing the Solr response, with no facets, and only the id and title_full_display field for each Solr doc
+def solr_resp_ids_full_titles_from_query(query)
+  resp = solr_resp_ids_full_titles({'q'=> query})
+end
+
 # send a GET request to the default Solr request handler with the indicated Solr parameters
 # @param solr_params [Hash] the key/value pairs to be sent to Solr as HTTP parameters, in addition to 
 #  those to get only id fields and no facets in the response
@@ -37,6 +51,14 @@ end
 # @return [RSpecSolr::SolrResponseHash] object for rspec-solr testing the Solr response, with no facets, and only the id and short title field for each Solr doc
 def solr_resp_ids_titles(solr_params)
   solr_response(solr_params.merge(doc_ids_short_titles))
+end
+
+# send a GET request to the default Solr request handler with the indicated Solr parameters
+# @param solr_params [Hash] the key/value pairs to be sent to Solr as HTTP parameters, in addition to 
+#  those to get only id fields and title_full_display and no facets in the response
+# @return [RSpecSolr::SolrResponseHash] object for rspec-solr testing the Solr response, with no facets, and only the id and full title field for each Solr doc
+def solr_resp_ids_full_titles(solr_params)
+  solr_response(solr_params.merge(doc_ids_full_titles))
 end
 
 def author_search_args(query_str)
@@ -72,6 +94,7 @@ end
 # response documents will only have id fields, and there will be no facets in the response
 @@doc_ids_only = {'fl'=>'id', 'facet'=>'false'}
 @@doc_ids_short_titles = {'fl'=>'id,title_245a_display', 'facet'=>'false'}
+@@doc_ids_full_titles = {'fl'=>'id,title_full_display', 'facet'=>'false'}
 
 # response documents will only have id fields, and there will be no facets in the response
 # @return [Hash] Solr HTTP params to reduce the size of the Solr responses
@@ -83,6 +106,12 @@ end
 # @return [Hash] Solr HTTP params to reduce the size of the Solr responses
 def doc_ids_short_titles
   @@doc_ids_short_titles
+end
+
+# response documents will only have id and title_full_display fields, and there will be no facets in the response
+# @return [Hash] Solr HTTP params to reduce the size of the Solr responses
+def doc_ids_full_titles
+  @@doc_ids_full_titles
 end
 
 def solr_schema
