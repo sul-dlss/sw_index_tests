0329db08f7a09c9f7dc49245a5fabde4a604cc18
Naomi Dushay <ndushay@stanford.edu>
2013-07-08 12:00:22 -0700

    added SW-623 IEEE xplore and subject NOT congresses to adv search spec

--- spec/advanced_search_spec.rb
+++ spec/advanced_search_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "advanced search" do
-  context "street art OR graffit", :jira => 'VUF-1013' do
+  context "street art OR graffiti", :jira => 'VUF-1013' do
     it "subject ((street art) OR graffiti OR mural)" do
       resp = solr_response({'q'=>"#{subject_query('(+street +art) OR graffiti OR mural')}"}.merge(solr_args))
       resp.should have_at_least(1000).results
@@ -22,6 +22,33 @@ describe "advanced search" do
       resp = solr_response({'q'=>"#{subject_query('(+"street art") OR graffiti OR mural')} AND #{description_query('+chicano')}"}.merge(solr_args))
       resp.should include(['3034294','525462','3120734','1356131','7746467'])
       resp.should have_at_most(10).results
+    end
+  end
+  
+  
+  context "subject -congresses, keyword IEEE xplore", :jira => 'SW-623' do
+    it "subject (-congresses)", :fixme => true do
+      # advanced qp currently doesn't support hyphen as NOT
+      resp = solr_response({'q'=>"#{subject_query('(-congresses)')}"}.merge(solr_args))
+      resp.should have_at_least(200000).results
+    end
+    it "subject NOT congresses" do
+      resp = solr_response({'q'=>"NOT #{subject_query('congresses')}"}.merge(solr_args))
+      resp.should have_at_least(200000).results
+    end
+    it "keyword IEEE xplore" do
+      resp = solr_response({'q'=>"#{description_query('+IEEE +xplore')}"}.merge(solr_args))
+      resp.should have_the_same_number_of_results_as(solr_resp_ids_from_query("IEEE Xplore"))
+      #  optional terms (no +)
+      resp.should have_fewer_results_than(solr_response({'q'=>"#{description_query('IEEE xplore')}"}.merge(solr_args)))
+      resp.should have_at_least(6000).results
+      resp.should have_at_most(7250).results
+    end
+    it "subject NOT congresses and keyword IEEE xplore" do
+      resp = solr_response({'q'=>"NOT #{subject_query('congresses')} AND #{description_query('+IEEE +xplore')}"}.merge(solr_args))
+      resp.should have_at_least(800).results  # in situ, production
+      # not true in Solr 3.6  or Solr 4.3 dismax
+      resp.should have_at_most(900).results
     end
   end
   
