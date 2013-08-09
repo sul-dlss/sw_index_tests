599fbb89ca6ece404e9bc296747a492abe908dd1
Naomi Dushay <ndushay@stanford.edu>
2013-08-09 13:15:52 -0700

    refactoring chinese_old_fiction

--- spec/cjk/chinese_old_fiction_spec.rb
+++ spec/cjk/chinese_old_fiction_spec.rb
@@ -4,9 +4,10 @@ require 'rspec-solr'
 
 describe "Chinese Old Fiction:  舊 (old) and  小說 (fiction)", :chinese => true, :fixme => true, :wordbreak => true, :vetted => 'vitus' do
 
-  shared_context "old fiction results" do
-    shared_examples "great search results for 舊小說" do
-      describe do
+  context "old fiction" do
+    shared_examples_for "great search results for 舊小說" do
+#    shared_examples_for "great search results for 舊小說" do |resp|
+#      describe do
         it "gets the traditional char matches" do
           # the first 4 lines are redundant, but split out for ease of diagnostics
           resp.should include(["9262744", "6797638", "6695967"]) # in 245a
@@ -48,31 +49,33 @@ describe "Chinese Old Fiction:  舊 (old) and  小說 (fiction)", :chinese => tr
           #   6698466 - old 245a; fiction 490a, 830a
           resp.should include(["6288832", "7699186", "6204747", "6698466"])
         end
-      end # describe
+#      end # describe
     end
-  end # shared_context old fiction results
 
   context "title search" do
-    shared_context "ts" do
+#    shared_context "ts" do
       shared_examples "great title search results for 舊小說" do
-        describe do
+#      shared_examples_for "great title search results for 舊小說" do |resp|
+#        describe do
           it "gets a reasonable number of results" do
             resp.should have_at_least(8).documents
             resp.should have_at_most(15).documents
           end
-        end 
-      end
+#        end 
+#      end
     end
-    include_context "old fiction results"
-    include_context "ts"
+#    include_context "old fiction results"
+#    include_context "ts"
 
     context "traditional  舊小說 no spaces" do
       before(:all) do
         @resp = solr_resp_doc_ids_only(title_search_args('舊小說').merge({'rows'=>'25'}))
       end
+#      it_behaves_like "great search results for 舊小說", @resp
       it_behaves_like "great search results for 舊小說" do
         let (:resp) { @resp }
       end
+#      it_behaves_like "great title search results for 舊小說", @resp
       it_behaves_like "great title search results for 舊小說" do
         let (:resp) { @resp }
       end
@@ -82,6 +85,8 @@ describe "Chinese Old Fiction:  舊 (old) and  小說 (fiction)", :chinese => tr
       before(:all) do
         @resp = solr_resp_doc_ids_only(title_search_args('舊 小說').merge({'rows'=>'25'}))
       end
+#      it_behaves_like "great search results for 舊小說", @resp
+#      it_behaves_like "great title search results for 舊小說", @resp
       it_behaves_like "great search results for 舊小說" do
         let (:resp) { @resp }
       end
@@ -94,6 +99,8 @@ describe "Chinese Old Fiction:  舊 (old) and  小說 (fiction)", :chinese => tr
       before(:all) do
         @resp = solr_resp_doc_ids_only(title_search_args('旧小说').merge({'rows'=>'25'}))
       end
+#      it_behaves_like "great search results for 舊小說", @resp
+#      it_behaves_like "great title search results for 舊小說", @resp
       it_behaves_like "great search results for 舊小說" do
         let (:resp) { @resp }
       end
@@ -106,6 +113,8 @@ describe "Chinese Old Fiction:  舊 (old) and  小說 (fiction)", :chinese => tr
       before(:all) do
         @resp = solr_resp_doc_ids_only(title_search_args('旧 小说').merge({'rows'=>'25'}))
       end
+#      it_behaves_like "great search results for 舊小說", solr_resp_doc_ids_only(title_search_args('旧 小说').merge({'rows'=>'25'}))
+#      it_behaves_like "great title search results for 舊小說", solr_resp_doc_ids_only(title_search_args('旧 小说').merge({'rows'=>'25'}))
       it_behaves_like "great search results for 舊小說" do
         let (:resp) { @resp }
       end
@@ -117,22 +126,25 @@ describe "Chinese Old Fiction:  舊 (old) and  小說 (fiction)", :chinese => tr
   end # context title search
 
   context "everything search" do
-    shared_context "es" do
+#    shared_context "es" do
       shared_examples "great everything search results for 舊小說" do
-        describe do
+#      shared_examples_for "great everything search results for 舊小說" do |resp|
+#        describe do
           it "gets a reasonable number of results" do
             resp.should have_at_least(20).documents
             resp.should have_at_most(40).documents
           end
-        end 
+#        end 
       end
-    end
-    include_context "es"
+#    end
+#    include_context "es"
 
     context "traditional  舊小說 no spaces" do
       before(:all) do
         @resp = solr_resp_doc_ids_only({'q'=>'舊小說', 'rows'=>'25'})
       end
+#      it_behaves_like "great search results for 舊小說", solr_resp_doc_ids_only({'q'=>'舊小說', 'rows'=>'25'})
+#      it_behaves_like "great everything search results for 舊小說", solr_resp_doc_ids_only({'q'=>'舊小說', 'rows'=>'25'})
       it_behaves_like "great search results for 舊小說" do
         let (:resp) { @resp }
       end
@@ -145,6 +157,8 @@ describe "Chinese Old Fiction:  舊 (old) and  小說 (fiction)", :chinese => tr
       before(:all) do
         @resp = solr_resp_doc_ids_only({'q'=>'舊 小說', 'rows'=>'25'})
       end
+#      it_behaves_like "great search results for 舊小說", solr_resp_doc_ids_only({'q'=>'舊 小說', 'rows'=>'25'})
+#      it_behaves_like "great everything search results for 舊小說", solr_resp_doc_ids_only({'q'=>'舊 小說', 'rows'=>'25'})
       it_behaves_like "great search results for 舊小說" do
         let (:resp) { @resp }
       end
@@ -157,6 +171,8 @@ describe "Chinese Old Fiction:  舊 (old) and  小說 (fiction)", :chinese => tr
       before(:all) do
         @resp = solr_resp_doc_ids_only({'q'=>'旧小说', 'rows'=>'25'})
       end
+#      it_behaves_like "great search results for 舊小說", solr_resp_doc_ids_only({'q'=>'旧小说', 'rows'=>'25'})
+#      it_behaves_like "great everything search results for 舊小說", solr_resp_doc_ids_only({'q'=>'旧小说', 'rows'=>'25'})
       it_behaves_like "great search results for 舊小說" do
         let (:resp) { @resp }
       end
@@ -169,6 +185,8 @@ describe "Chinese Old Fiction:  舊 (old) and  小說 (fiction)", :chinese => tr
       before(:all) do
         @resp = solr_resp_doc_ids_only({'q'=>'旧 小说', 'rows'=>'25'})
       end
+#      it_behaves_like "great search results for 舊小說", solr_resp_doc_ids_only({'q'=>'旧 小说', 'rows'=>'25'})
+#      it_behaves_like "great everything search results for 舊小說", solr_resp_doc_ids_only({'q'=>'旧 小说', 'rows'=>'25'})
       it_behaves_like "great search results for 舊小說" do
         let (:resp) { @resp }
       end
@@ -178,5 +196,7 @@ describe "Chinese Old Fiction:  舊 (old) and  小說 (fiction)", :chinese => tr
     end
     
   end # context everything search
+
+  end # context old fiction
 
 end
