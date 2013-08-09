4d10559812c061a335e6b1bbd5cfcc2fd30a2678
Naomi Dushay <ndushay@stanford.edu>
2013-08-09 12:30:52 -0700

    working on three kingdoms spec in chinese_unigram_spec

--- spec/cjk/chinese_unigram_spec.rb
+++ spec/cjk/chinese_unigram_spec.rb
@@ -6,11 +6,60 @@ describe "Chinese: Word Breaks - Unigrams", :chinese => true, :fixme => true, :w
   # see chinese_old_fiction_spec  for  "舊小說 (old fiction) becomes  舊 (unigram) and  小說 (bigram)
   # see women_and_literature_spec for  "婦女與文學 becomes  婦女 (women)  與 (and)   文學 (literature)
   
-  context " 三國誌 becomes  三國 (bigram) and  誌 (unigram)" do
-    it "三國誌 (no spaces) should have good search results" do
-      resp = solr_resp_doc_ids_only({'q'=>'三國誌', 'rows'=>'20'}) 
-      resp.should include(["6343505", "8246653"]).as_first(2).documents
-      resp.should include(["4724601", "8250645", "6343719", "6343720"]).in_first(6).documents  # script as is
+  context "Three kingdoms 三國誌 becomes  三國 (three - bigram) and  誌 (unigram)" do
+    shared_examples_for "great results for Three kingdoms" do |resp, num_exp|
+      it "should have the right number of results" do
+        resp.should have_at_least(num_exp).documents  
+      end
+      context "should have the best results first" do
+        it "San guo zhi by Chen Shou  (exact 245a)" do
+          resp.should include(['6696113', # microform, EAL
+                                '9265099', # v. 394  harvard-yenching coll, sal1&2
+                                '5630782', # v1-5  EAL Chinese coll
+                                '9424180', # v 487  harvard-yenching coll, sal1&2
+                                '8656531', # ser2: v14  sal1&2
+                                '8656623', # ser2:v106 sal1&2
+                                '9407164', # ser2:v53 harvard-yenching coll, sal1&2
+                                '6718129', # v254 east asia big sets sal1&2
+                                '6435402', # 65 juan  v1-5  harvard-yenching coll, sal1&2
+                                '6829978', # 65 juan  v1-4  EAL chinese coll
+                                '10102192', # v96-97  sal1&2
+                                '8656532',  # ser2 v15  sal1&2
+                                '6435401'   # v1-16  harvard-yenching coll, sal1&2
+                                ])
+        end
+        it "Samgukchi (Korean)  (exact 245a)" do
+          resp.should include(['8303152', # Samgukchi / Na Kwan-jung chiŭm  author Luo, Guanzhong  (245a, first 3 chars of 240a, 700t)
+                                '10156316', # Samgukchi by  Yi Mun-yŏl p'yŏngyŏk  (245a, part of 246a, 500a, 700t)
+                                ])
+        end
+        it "Sangokushi (Japanese)   (exact 245a, but simplified 三国志)" do
+          resp.should include(['6627151', # Sangokushi / [Ra Kanchū sen]  (245a, first 3 chars of 240a, 500a)
+                                '9146942', # Sangokushi. by Yokoyama, Mitsuteru
+                                ])
+        end
+#        it "should give high rank to traditional chars 三國誌 exact 245a match" do
+#        end
+#        it "should give high rank to simplified chars 三国志 exact 245a match" do
+#        end
+#        it "should match other highly boosted fields" do
+#          #  8448620 - 三国志 240a,  start of 245a, 600t
+#        end
+      end # best results first
+    end # shared_examples_for  great results for Three kingdoms 
+    
+    # simplified  三国志
+    
+    context "三國誌 (trad, no spaces) should have good search results" do
+      it_behaves_like "great results for Three kingdoms", solr_resp_ids_from_query('三國誌'), 170
+#      resp = solr_resp_doc_ids_only({'q'=>'三國誌', 'rows'=>'20'}) 
+      # soc:  170 as of 2013-08-08
+      # 三國誌 (no spaces)  soc:  170
+      #   231
+      # 
+      # 三國 誌 (spaces)  soc:  170
+      #   253
+      # 
       # soc:                           242 
       # cjk1 (bigrams and unigrams):   365
       # cjk3 (bigrams only):           365
@@ -18,6 +67,7 @@ describe "Chinese: Word Breaks - Unigrams", :chinese => true, :fixme => true, :w
       # cjk6cn (chinese dict only):    218
       # cjk8 (chinese dict no pos):    218
     end
+
     it "三國 誌 (space) should have good search results" do
       resp = solr_resp_doc_ids_only({'q'=>'三國 誌', 'rows'=>'20'}) 
       # soc:                           247
@@ -53,7 +103,7 @@ describe "Chinese: Word Breaks - Unigrams", :chinese => true, :fixme => true, :w
     # positionfilter ...
     # overlapping tokens ...   what if they didn't overlap and were ANDed
     end # title search
-  end
+  end #  Three kingdoms
   
   context "飘  float/drifting (Gone with the wind)" do
     shared_examples_for "great results for float (Gone with the Wind)" do |query|
@@ -62,10 +112,15 @@ describe "Chinese: Word Breaks - Unigrams", :chinese => true, :fixme => true, :w
       end
       it "should have the right number of results" do
         @resp.should have_at_least(120).documents  # soc:  120 as of 2013-08-08
+        @resp.should have_at_most(150).documents
       end
-      it "should have the best results first" do
-        @resp.should include('6701323').as_first # Gone with the Wind (book)
-        @resp.should include('7737681').in_first(3) # Drifting (video)
+      context "should have the best results first" do
+        it "Gone with the Wind (book)" do
+          @resp.should include('6701323').as_first
+        end
+        it "Drifting (video)" do
+          @resp.should include('7737681').in_first(3)
+        end
       end
     end
     context "traditional:  飄" do
