d12752f0614188833041d984478ce2ba6a69cb9e
Naomi Dushay <ndushay@stanford.edu>
2013-08-14 19:32:18 -0700

    a little more refactoring of chinese_old_fiction spec

--- spec/cjk/chinese_old_fiction_spec.rb
+++ spec/cjk/chinese_old_fiction_spec.rb
@@ -2,7 +2,11 @@
 require 'spec_helper'
 require 'rspec-solr'
 
-describe "Chinese Old Fiction:  舊 (old) and  小說 (fiction)", :chinese => true, :fixme => true, :wordbreak => true, :vetted => 'vitus' do
+describe "Chinese Old Fiction:  舊 (old) and  小說 (fiction)", :chinese => true, :fixme => true, :vetted => 'vitus' do
+  # old (simp)  旧
+  # old (trad)  舊
+  # fiction (simp)  小说
+  # fiction (trad)  小說
 
   shared_examples_for "great search results for 舊小說" do
     it "gets the traditional char matches" do
@@ -15,36 +19,31 @@ describe "Chinese Old Fiction:  舊 (old) and  小說 (fiction)", :chinese => tr
       resp.should include("6695904").before("6699444")
     end
     it "gets the simplified char matches" do
-      # 旧小说
-      resp.should include("8834455") #  旧小说 in 245a
+      resp.should include("8834455") # all three chars in 245a
       resp.should include("4192734")  # diff word order in 245a  小说旧
       resp.should include("8834455").before("4192734")
     end
     it "ranks highest the documents with adjacent words in 245a" do
-      #   9262744 - old fiction: 245a
-      #   6797638 - old fiction: 245a
-      #   6695967 - old fiction: 245a
-      #   8834455 - translated to simplified, all three in 245a
-      resp.should include(["9262744", "6797638", "6695967", "8834455"]).in_first(5).results
+      resp.should include("9262744").in_first(5).results # old fiction: 245a
+      resp.should include("6797638").in_first(5).results # old fiction: 245a
+      resp.should include("6695967").in_first(5).results # old fiction: 245a
+      resp.should include("8834455").in_first(5).results # translated to simplified, all three in 245a
     end
     it "ranks very high the documents with both words in 245a but not adjacent" do
-      #   4192734 - diff word order in 245a  小说旧
-      resp.should include(["4192734"]).in_first(6).results
+      resp.should include(["4192734"]).in_first(6).results #  # diff word order in 245a  小说旧
     end
     it "ranks high the documents with one word in 245a and the other in 245b" do
-      #   6695904 - fiction 245a; old 245a
-      #   6699444 - old 245a; fiction 245b
-      #   6696790 - old 245a; fiction 245a
-      #   7198256 - old 245b; fiction: 245a  (Korean also in record)
-      #   6793760 - old (simplified) 245a; fiction 245b
-      resp.should include(["6695904", "6699444", "6696790", "7198256", "6793760"]).in_first(12).results
+      resp.should include("6695904").in_first(12).results # fiction 245a; old 245a
+      resp.should include("6699444").in_first(12).results # old 245a; fiction 245b
+      resp.should include("6696790").in_first(12).results # old 245a; fiction 245a
+      resp.should include("7198256").in_first(12).results # old 245b; fiction: 245a  (Korean also in record)
+      resp.should include("6793760").in_first(12).results # old (simplified) 245a; fiction 245b
     end
     it "includes other relevant results" do
-      #   6288832 - old 505t; fiction 505t x2
-      #   7699186 - 1st character in simplified in 245a, 2nd and 3rd in 490 and 830, 3rd character in simplified
-      #   6204747 - old 245a; fiction 490a; 830a
-      #   6698466 - old 245a; fiction 490a, 830a
-      resp.should include(["6288832", "7699186", "6204747", "6698466"])
+      resp.should include("6288832") # old 505t; fiction 505t x2
+      resp.should include("7699186") # old (simp) in 245a, fiction (simp) in 490 and 830
+      resp.should include("6204747") # old 245a; fiction 490a; 830a
+      resp.should include("6698466") # old 245a; fiction 490a, 830a
     end
   end # shared examples  great search results for 舊小說
 
