90df89b648c221aa70614b9cd5e58aaa2694b649
Naomi Dushay <ndushay@stanford.edu>
2013-08-14 19:46:49 -0700

    twiddle comments chinese_variant_chars

--- spec/cjk/chinese_variant_characters_spec.rb
+++ spec/cjk/chinese_variant_characters_spec.rb
@@ -5,10 +5,10 @@ require 'rspec-solr'
 describe "Chinese: Variant Characters", :chinese => true, :fixme => true do
   
   it "囯 vs  国" do
-    resp = solr_resp_doc_ids_only({'q'=>'民囯时期社会调查丛编'}) # A (囯) is second char  # 1 in prod: 8593449, 2 in soc
+    resp = solr_resp_doc_ids_only({'q'=>'民囯时期社会调查丛编'}) # A (囯) is second char  # 1 in prod: 8593449, 2 in soc as of 2012-11
     resp.should have_at_least(3).documents
     resp.should include(["8593449", "6553207"])
-    resp.should include("8940619")  # has title 民国时期社会调查丛编 - B (国) is second char  # 1 in prod: 8940619, 1 in soc
+    resp.should include("8940619")  # has title 民国时期社会调查丛编 - B (国) is second char  # 1 in prod: 8940619, 1 in soc as of 2012-11
     pending("need to implement regex matcher to look for  民国时期社会调查丛编  in vern_title_display-ish")
   end
 
