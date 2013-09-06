cc98cf6bbfddb36297412ce0af8a0296a8c9f00b
Naomi Dushay <ndushay@stanford.edu>
2013-09-06 14:17:05 -0700

    upgrading japanese_overview spec

--- spec/cjk/japanese_overview_spec.rb
+++ spec/cjk/japanese_overview_spec.rb
@@ -129,93 +129,88 @@ describe "Japanese Overview", :japanese => true, :fixme => true do
   end # title searches
 
   context "author searches" do
-    context "South Manchurian Railroad Company" do
-      it_behaves_like "both scripts get expected result size", 'author', 'modern', ' 南満州鉄道株式会社', 'traditional', '南滿洲鐵道株式會社', 522
-    end
-    context "personal name  契沖" do
-      it_behaves_like "both scripts get expected result size", 'author', 'kanji', '契沖', 'hiragana', 'けいちゅう', 5
-    end
-    context "kanji surname  釘貫 (surname of 釘貫亨)" do
-      it_behaves_like "expected result size", '釘貫', 'author', 1
-      it_behaves_like "expected result size", '釘貫亨', 'author', 1
+    context "buddhism", :jira => 'VUF-2723' do
+      it_behaves_like "both scripts get expected result size", 'author', 'traditional', '佛教', 'modern', '仏教', 96, 300
     end
     context "高橋 (common personal name)", :jira => 'VUF-2711' do
-      it_behaves_like "expected result size", '高橋', 'author', 1045
+      it_behaves_like "expected result size", 'author', '高橋', 1021, 1050
     end
-    context "buddhism, 2 char", :jira => ['VUF-2724', 'VUF-2725'] do
-      it_behaves_like "both scripts get expected result size", 'author', 'traditional', '佛教', 'script2', '仏教', 'modern', 182
+    context "personal name  契沖" do
+      it_behaves_like "both scripts get expected result size", 'author', 'kanji', '契沖', 'hiragana', 'けいちゅう', 5, 5
+    end
+    context "personal name: kanji surname  釘貫 (surname of 釘貫亨)" do
+      it_behaves_like "expected result size", 'author', '釘貫', 1, 1
+      it_behaves_like "expected result size", 'author', '釘貫亨', 1, 1
+    end
+    context "South Manchurian Railroad Company", :jira => ['VUF-2736', 'VUF-2739'] do
+      it_behaves_like "both scripts get expected result size", 'author', 'modern', ' 南満州鉄道株式会社', 'traditional', '南滿洲鐵道株式會社', 400, 700
     end
   end # author searches
   
   context "everything searches" do
-    context "manchuria kanji   満洲", :jira => 'VUF-2713' do
-      it_behaves_like "expected result size", "満洲", nil, 404
-    end
-    context "manchuria katakana  マンチュリヤ", :jira => 'VUF-2712' do
-      it_behaves_like "expected result size", ' マンチュリヤ', nil, 1
-    end
-    context "TPP", :jira => 'VUF-2694' do
-      it_behaves_like "expected result size", 'TPP', nil, 69
-    end
-    context "publisher's name  日外アソシエーツ", :jira => 'VUF-2699' do
-      it_behaves_like "expected result size", '日外アソシエーツ', nil, 455
-    end
-    context "outside of Japan 日外" do
-      it_behaves_like "expected result size", '日外', nil, 2411
-    end
-    context '(local/regional society) 地域社会', :jira => 'VUF-2717' do
-      it_behaves_like "expected result size", '"地域社会', nil, 400
+    context "buddhism", :jira => ['VUF-2724', 'VUF-2725'] do
+      it_behaves_like "both scripts get expected result size", 'everything', 'traditional', '佛教', 'modern', '仏教', 814, 2000
     end
     context 'Edo (old name for Tokyo)', :jira => 'VUF-2726' do
-      it_behaves_like "both scripts get expected result size", 'script1', '江戶', 'script2', '江戸', 'everything', 3
+      it_behaves_like "both scripts get expected result size", 'everything', 'traditional', '江戶', 'modern', '江戸', 3, 2000
     end
-    context "investigation", :jira => 'VUF-2727' do
-      it_behaves_like "both scripts get expected result size", 'script1', '調查', 'script2', '調査', 'everything', 11139
+    context "Japan   China   thought", :jira => 'VUF-2737' do
+      it_behaves_like "expected result size", 'everything', '日本   中国   思想', 52, 55
     end
-    context "buddhism", :jira => ['VUF-2724', 'VUF-2725'] do
-      it_behaves_like "both scripts get expected result size", 'traditional', '佛教', 'script2', '仏教', 'modern', 'everything', 2336
+    context '(local/regional society)', :jira => 'VUF-2717' do
+      it_behaves_like "expected result size", 'everything', '"地域社会', 400, 425
     end
-    context "seven wonders  七不思議", :jira => 'VUF-2710' do
-      it_behaves_like "expected result size", '七不思議', nil, 3, 3
+    context "manchuria" do
+      it_behaves_like "both scripts get expected result size", 'everything', 'katakana', 'マンチュリヤ', 'kanji', '満洲', 350, 400
+      context "katakana", :jira => 'VUF-2712' do
+        it_behaves_like "expected result size", 'everything', ' マンチュリヤ', 1, 2
+      end
+      context "kanji", :jira => 'VUF-2713' do
+        it_behaves_like "expected result size", 'everything', "満洲", 362, 415
+      end
+    end
+    context "outside of Japan", :jira => 'VUF-2699' do
+      it_behaves_like "expected result size", 'everything', '日外', 2000, 2450
+    end
+    context "publisher's name starting with 'outside of Japan'", :jira => 'VUF-2699' do
+      it_behaves_like "expected result size", 'everything', '日外アソシエーツ', 455, 525
+    end
+    context "seven wonders", :jira => 'VUF-2710' do
+      it_behaves_like "expected result size", 'everything', '七不思議', 3, 3
     end
     context "seven wonders of sassafras springs  ササフラス・スプリングスの七不思議", :jira => 'VUF-2709' do
-      it_behaves_like "expected result size", 'ササフラス・スプリングスの七不思議', nil, 1, 1
+      it_behaves_like "expected result size", 'everything', 'ササフラス・スプリングスの七不思議', 1, 1
     end
-    
-    context "phrases" do
-      context '”地域社会から見る”', :jira => 'VUF-2716' do
-        # want 10221504, but dont get it ...
-        it_behaves_like "expected result size", '”地域社会から見る”', nil, 3
-      end
-      context '"地域社会から見る帝国日本と植民地"', :jira => 'VUF-2714' do
-        it_behaves_like "expected result size", '"地域社会から見る帝国日本と植民地"', nil, 1
-      end
-      context '(local/regional society) "地域社会"', :jira => 'VUF-2717' do
-        it_behaves_like "expected result size", '""地域社会"', nil, 266
-      end
+    context "survey/investigation", :jira => 'VUF-2727' do
+      it_behaves_like "both scripts get expected result size", 'everything', 'traditional', ' 調查', 'modern', '調査', 100, 12000
     end
+    context "TPP", :jira => 'VUF-2694' do
+      it_behaves_like "expected result size", 'everything', 'TPP', 69, 80
+    end    
   end # everything searches
   
-  context "mixed scripts" do
-    context "title searches" do
-      context "Japanese art works reference encyclopedia  日本美術作品レファレンス事典", :jira => 'VUF-2698' do
-        it_behaves_like "expected result size", '日本美術作品レファレンス事典', 9
-      end
-      context "modern pseudonym usage  近世仮名遣い" do
-        it_behaves_like "expected result size", '近世仮名遣い', 'title', 1
-      end
-      context "'Blocking' ブロック化 katakana-kanji mix", :jira => 'VUF-2695' do
-        it_behaves_like "expected result size", 'ブロック化', 'title', 1
-      end
-      context "twin story of the swan  mixed hiragana/katakana", :jira => 'VUF-2704' do
-        it_behaves_like "expected result size", '白鳥のふたごものがたり', 'title', 1
-      end
-      context "people traveling overseas encyclopedia", :jira => 'VUF-2084' do
-        it_behaves_like "expected result size", '海外渡航者人物事典', 'title', 1
-        # FIXME:  the ticket also talks about needing to match substrings of title
-        # ckey 5746725
-      end
+  context "phrases" do
+    context '(local/regional society)', :jira => 'VUF-2717' do
+      it_behaves_like "expected result size", 'everything', '""地域社会"', 266, 300
     end
+    context '(local/regional society - I see from)', :jira => 'VUF-2716' do
+      # TODO:  want 10221504, but dont get it ...
+      it_behaves_like "expected result size", 'everything', '”地域社会から見る”', 3, 3
+    end
+    context '(local/regional society - all of Japan)', :jira => 'VUF-2714' do
+      it_behaves_like "expected result size", 'everything', '"地域社会から見る帝国日本と植民地"', 1, 1
+    end
+  end  # phrases
+  
+  context "mixed scripts" do
+    # katakana-kanji:  see 'blocking' title search, :jira => 'VUF-2695'
+    # hiragana-kanji:  see 'lantern shop' title search, :jira => 'VUF-2702'
+    # modern mixed:  see  pseudonum title search  近世仮名遣い
+    # hiragana, kanji, katakana:  see tsu child remains lantern shop title search  ちょうちん屋のままッ子, :jira => 'VUF-2701'
+    # hiragana-katakana:  see twin story of the swan title search 白鳥のふたごものがたり, :jira=> 'VUF-2704
+    # seven wonders everything search 七不思議  VUF-2710
+    # seven wonders of sassafras springs everything search  ササフラス・スプリングスの七不思議  VUF-2709
+
   end # mixed scripts
 
 end
