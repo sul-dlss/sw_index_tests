8086791ba2b63d8b72180f3c03bea0d87a9ada48
Naomi Dushay <ndushay@stanford.edu>
2013-09-08 10:14:10 -0700

    update sort spec

--- spec/sort_spec.rb
+++ spec/sort_spec.rb
@@ -7,16 +7,15 @@ describe "sorting results" do
       resp = solr_response({'fl'=>'id,pub_date', 'facet'=>false})
       resp.should_not include('1') # should not be in document id order
       year = Time.new.year
-      # TODO:  waiting for gdor fix to Walters pub_date_sort values
-  #    resp.should include("pub_date" => /(#{year}|#{year + 1}|#{year + 2})/).in_each_of_first(20).documents
+      resp.should include("pub_date" => /(#{year}|#{year + 1}|#{year + 2})/).in_each_of_first(20).documents
     end
 
     it "with facet format:Book; default sort should be by pub date desc then title asc" do
       resp = solr_response({'fq'=>'format:Book', 'fl'=>'id,pub_date', 'facet'=>false})
       year = Time.new.year
       resp.should include("pub_date" => /(#{year}|#{year + 1}|#{year + 2})/).in_each_of_first(20).documents
-      # _Applications of Microsoft Excel in analytical chemistry_  before  _Chemistry : a molecular approach_
-      resp.should include('9937771').before('9937759')
+      # _Action research_ (2014, Cubberley) before _Africa's discovery of Europe_ (2014, Green)
+      resp.should include('10094485').before('10145562')
     end
     
     it "with facet access:Online; default sort should be by pub date desc then title asc" do
