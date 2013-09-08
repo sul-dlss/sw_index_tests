fa56a375f1f404ebedf25a8115e69adf9875d100
Naomi Dushay <ndushay@stanford.edu>
2013-09-08 10:13:49 -0700

    Lane ckey for Nature changed

--- spec/journal_title_spec.rb
+++ spec/journal_title_spec.rb
@@ -602,7 +602,7 @@ describe "journal/newspaper titles" do
     end
 
     it_behaves_like "great results for format journal", "Nature" do
-      journal = ['10189796', # london 0028-0836, lane/medical
+      journal = ['10237615', # london 0028-0836, lane/medical
                   '3195844', # london 0028-0836, biology
                   '8829478', # london, spec
                   '466281', # directory of biologicals
