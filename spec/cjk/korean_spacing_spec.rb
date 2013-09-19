15fbae920cf09187e713b5dcd7a2ae4ff1bdd08d
Naomi Dushay <ndushay@stanford.edu>
2013-09-19 16:02:09 -0700

    move some specs out of korean_spacing into other files

--- spec/cjk/korean_spacing_spec.rb
+++ spec/cjk/korean_spacing_spec.rb
@@ -3,8 +3,6 @@ require 'spec_helper'
 
 describe "Korean spacing", :korean => true do
   
-  lang_limit = {'fq'=>'language:Korean'}
-
   context "Korea-U.S. alliance relations in the 21st century", :jira => 'VUF-2747' do
     shared_examples_for "good title results for 21세기의  한미동맹관계" do | query |
       it_behaves_like 'good results for query', 'title', query, 1, 6, '8375648', 1
@@ -56,40 +54,6 @@ describe "Korean spacing", :korean => true do
       end
     end
   end   # Korea-U.S. alliance relations in the 21st century  VUF-2747
-
-
-  context "author: Ŭn, Hŭi-gyŏng", :jira => 'VUF-2729' do
-    shared_examples_for "good author results for 은희경" do | query |
-      #  "top 15 results are all relevant for searches both with and without whitespace between author’s last and first name."
-      relevant = ['9628681',
-                  '8814719',
-                  '7874461',
-                  '8299426',
-                  '9097174',
-                  '8532805',
-                  '8823369',
-                  '7874535',
-                  '7890077',
-                  '7868363',
-                  '7849970',
-                  '8827330',
-                  '7880024',
-                  '7841550',
-                  '6972331',
-                ]
-      it_behaves_like 'good results for query', 'author', query, 15, 30, relevant, 20
-      irrelevant = ['10150829', # has one contributor with last name, another contributor with first name
-                    '9588786',  # has 3 chars jumbled in 710:  경희 대학교. 밝은 사회 연구소
-                    ]
-      it_behaves_like 'does not find irrelevant results', 'author', query, irrelevant
-    end
-    context "은희경 (no spaces)" do
-      it_behaves_like "good author results for 은희경", '은희경'
-    end
-    context "은 희경 (spaces:  은: last name  희경:first name)" do
-      it_behaves_like "good author results for 은희경", '은희경'
-    end
-  end # author: Ŭn, Hŭi-gyŏng  VUF-2729
 
 
   context "Kkum kkunŭn cha ka chʻangjo handa", :jira => 'VUF-2700' do
@@ -162,16 +126,6 @@ describe "Korean spacing", :korean => true do
       it_behaves_like "good results for 강물이될때까지", '강물 이 될 때 까지'
     end
   end # until the river  VUF-2744
-
-
-  context "phrases" do
-    context "Korea's modern history", :jira => 'VUF-2722', :fixme => true do
-      context '"“한국  근대사"' do
-        it_behaves_like "expected result size", 'title', '"“한국  근대사"', 1, 1
-        it_behaves_like "best matches first", 'title', '"“한국  근대사"', '8375648', 1
-      end
-    end
-  end  # phrases
   
   context "mixed scripts" do
   end # mixed scripts
