40bebe37a01f6e1daa5ab2ed5fda0b4c6831109d
Naomi Dushay <ndushay@stanford.edu>
2013-09-19 17:35:36 -0700

    finish up korean author spec, at least for now

--- spec/cjk/korean_author_spec.rb
+++ spec/cjk/korean_author_spec.rb
@@ -1,9 +1,9 @@
 # -*- encoding : utf-8 -*-
 require 'spec_helper'
 
-describe "Korean spacing", :korean => true do
+describe "Korean author", :korean => true do
   
-  context "Ŭn, Hŭi-gyŏng", :jira => 'VUF-2729' do
+  context "Ŭn, Hŭi-gyŏng  은희경", :jira => 'VUF-2729' do
     #  "top 15 results are all relevant for searches both with and without whitespace between author’s last and first name."
     relevant = ['9628681',
                 '8814719',
@@ -37,7 +37,7 @@ describe "Korean spacing", :korean => true do
   end # Ŭn, Hŭi-gyŏng  VUF-2729
 
 
-  context "Kang, In-ch'ŏl", :jira => 'VUF-2721' do
+  context "Kang, In-ch'ŏl  강인철", :jira => 'VUF-2721' do
     chars_together_in_100 = ['6914120', '6724932']
     chars_w_space_in_100_space = ['9954769',  # as  강 인철
                               '9927817',
@@ -74,7 +74,7 @@ describe "Korean spacing", :korean => true do
   end # Kang, In-ch'ŏl  VUF-2721
 
 
-  context "Han, Yŏng-u" do
+  context "Han, Yŏng-u  한영우" do
     shared_examples_for "good author results for 한영우" do | query |
       it_behaves_like "expected result size", 'author', query, 29, 35      
       #  7142656:   김영우 (different last name), and the character "한" appears in the "contributor" field, "한국 교육사 학회."
@@ -84,12 +84,28 @@ describe "Korean spacing", :korean => true do
         resp.should include({'vern_author_person_display' => /한[[:space:]]*영우/}).in_each_of_first(25)
       end
     end
+    shared_examples_for "good everything results for 한영우" do | query |
+      it_behaves_like "good author results for 한영우", query
+      # FIXME ...
+      # 5424507 245a:  "한・영우리문화용어집" means "Korean(한)-English(영) glossary of Korean culture
+      # it_behaves_like 'does not find irrelevant results', 'everything', query, '5424507', 'rows' => 30
+      # 6811565 (has ... 10년에대한영향평가와 우... in 245)
+      it_behaves_like 'does not find irrelevant results', 'everything', query, '6811565', 'rows' => 25
+    end
     context "author search" do
       context "한영우 (no spaces)" do
         it_behaves_like "good author results for 한영우", '한영우'
       end
       context "한 영우 (spaces: 한 last name, 영우 first name)" do
         it_behaves_like "good author results for 한영우", '한 영우'
+      end
+    end
+    context "everything search" do
+      context "한영우 (no spaces)" do
+        it_behaves_like "good everything results for 한영우", '한영우'
+      end
+      context "한 영우 (spaces: 한 last name, 영우 first name)" do
+        it_behaves_like "good everything results for 한영우", '한 영우'
       end
     end
   end # Han, Yŏng-u
