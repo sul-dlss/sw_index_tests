e7c2a34773bcfbb99ba2adb71056eaaa21aa4be1
Naomi Dushay <ndushay@stanford.edu>
2013-09-19 19:10:17 -0700

    migrate chinese_zhengzhou_geography spec to usable specs in cjk folder

--- spec/cjk/chinese_zhengzhou_geography_spec.rb
+++ spec/cjk/chinese_zhengzhou_geography_spec.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 require 'spec_helper'
 
-describe "Chinese: 郑州 (zhengzhou - a place in China)  地理 (geography)", :chinese => true, :vetted => 'vitus' do
+describe "Chinese: 郑州 (zhengzhou - a place in China)  地理 (geography)", :chinese => true do
 
   shared_examples_for "great search results in title for 郑州地理" do
     # zhengzhou  trad  鄭州
@@ -75,7 +75,7 @@ describe "Chinese: 郑州 (zhengzhou - a place in China)  地理 (geography)", :
       # soc:  郑州 地理  (simpl) 8 results
       # 518541  geography in 260b, 710b;   can't find zhengzhou
       # 119747:  zhengzhou trad  鄭州  in 245a;  can't find geography
-      it "should get a reasonable number of results", :fixme => true do
+      it "should get a reasonable number of results" do
         resp.should have_at_least(8).documents 
         resp.should have_at_most(20).documents
       end
@@ -86,16 +86,16 @@ describe "Chinese: 郑州 (zhengzhou - a place in China)  地理 (geography)", :
         resp.should include("9926680").in_first(2) # zhengzhou (simp) in 245b, 246a, 650z (mult); geography  245a
       end
       # there are a LOT more of these --> socrates finds 1953 of them.
-      it "should rank high the documents with one word in 245a", :fixme => true do
-        resp.should include("4203541").in_first(3).results  # geography in 245a 490a, 830a;  郑州 (zhengzhou) in 260a
+      it "should rank high the documents with one word in 245a" do
+        resp.should include("4203541").in_first(9).results  # geography in 245a 490a, 830a;  郑州 (zhengzhou) in 260a
         #  geography in 245a; zhengzhou in 260a
-        resp.should include("5809974").in_first(8).results # zhengzhou 260a; geography 245a
-        resp.should include("10111516").in_first(8).results # zhengzhou (simp) 260a;  geography 245a
-        resp.should include("4197620").in_first(8).results # zhengzhou 260a, geography 245a
-        resp.should include("4808042").in_first(8).results # zhengzhou 260a, geography 245a
-        resp.should include("6704074").in_first(8).results # zhengzhou 260a, geography 245a
+        resp.should include("5809974").in_first(9).results # zhengzhou 260a; geography 245a
+        resp.should include("10111516").in_first(9).results # zhengzhou (simp) 260a;  geography 245a
+        resp.should include("4197620").in_first(9).results # zhengzhou 260a, geography 245a
+        resp.should include("4808042").in_first(9).results # zhengzhou 260a, geography 245a
+        resp.should include("6704074").in_first(9).results # zhengzhou 260a, geography 245a
       end
-      it "should rank less high the documents with both words present but not in 245", :fixme => true do
+      it "should rank less high the documents with both words present but not in 245" do
         resp.should include("7833848") # zhengzhou 260a, geography 490a
         resp.should include("5456407") # zhengzhou in 260a,  geography in 260b, 710b
       end
@@ -124,7 +124,6 @@ describe "Chinese: 郑州 (zhengzhou - a place in China)  地理 (geography)", :
       end
     end
     context "simplified  郑州地理 (no space)" do
-      # soc:  2 as of 2012-11
       before(:all) do
         @resp = solr_resp_doc_ids_only({'q'=>'郑州地理'})
       end
@@ -136,7 +135,6 @@ describe "Chinese: 郑州 (zhengzhou - a place in China)  地理 (geography)", :
       end
     end
     context "simplified  郑州 地理 (space)" do
-      # soc:  8 as of 2012-11
       before(:all) do
         @resp = solr_resp_doc_ids_only({'q'=>'郑州 地理'})
       end
