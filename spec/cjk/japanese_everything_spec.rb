0bc9722ba68f3e323b583de0a14bbf0a3e3ae051
Naomi Dushay <ndushay@stanford.edu>
2013-09-23 13:19:43 -0700

    add Edo spec to japanese everything and han variants

--- spec/cjk/japanese_everything_spec.rb
+++ spec/cjk/japanese_everything_spec.rb
@@ -5,7 +5,7 @@ describe "Japanese Everything Searches", :japanese => true do
 
   lang_limit = {'fq'=>'language:Japanese'}
 
-  context "buddhism", :jira => ['VUF-2724', 'VUF-2725'] do
+  context "buddhism", :jira => 'VUF-2724' do
     # FIXME:  first character of traditional doesn't translate to first char of 
     #  modern with ICU traditional->simplified    (see also japanese han variants)
     context "traditional 佛教" do
@@ -24,9 +24,24 @@ describe "Japanese Everything Searches", :japanese => true do
     end
   end
   
-  context 'Edo (old name for Tokyo)', :jira => 'VUF-2726' do
-    it_behaves_like "both scripts get expected result size", 'everything', 'traditional', '江戶', 'modern', '江戸', 3, 2000
+  context 'Edo (old name for Tokyo)', :jira => ['VUF-2726', 'VUF-2770'] do
+    # FIXME:  second character of traditional doesn't translate to second char of 
+    #  modern with ICU traditional->simplified    (see also japanese han variants)
+    context "traditional 江戶" do
+      it_behaves_like "result size and vern short title matches first", 'everything', '江戶', 1900, 2000, /江戶/, 100
+      context "w lang limit" do
+        it_behaves_like "result size and vern short title matches first", 'everything', '江戶', 1900, 1950, /江戶/, 100, lang_limit
+      end
+    end
+    context "modern 江戸" do
+      it_behaves_like "expected result size", 'everything', '江戸', 3, 5
+      context "w lang limit" do
+        it_behaves_like "expected result size", 'everything', '江戸', 3, 5, lang_limit
+      end
+    end
   end
+  
+  
   context "Japan   China   thought", :jira => 'VUF-2737' do
     it_behaves_like "expected result size", 'everything', '日本   中国   思想', 52, 55
   end
--- spec/cjk/japanese_han_variants_spec.rb
+++ spec/cjk/japanese_han_variants_spec.rb
@@ -6,24 +6,41 @@ describe "Japanese Kanji variants", :japanese => true, :fixme => true do
   lang_limit = {'fq'=>'language:Japanese'}
 
   context "modern Kanji != simplified Han" do
-    context "Buddhism" do
-      # First character of traditional doesn't translate to first char of modern with ICU traditional->simplified 
-      context "buddhism", :jira => ['VUF-2724', 'VUF-2725'] do
-        it_behaves_like "both scripts get expected result size", 'everything', 'traditional', '佛教', 'modern', '仏教', 2200, 2300
+    context "buddhism", :jira => ['VUF-2724', 'VUF-2725'] do
+      # First char of traditional doesn't translate to first char of modern with ICU traditional->simplified 
+      it_behaves_like "both scripts get expected result size", 'everything', 'traditional', '佛教', 'modern', '仏教', 2200, 2300
+      # trad 佛教
+      it_behaves_like "result size and vern short title matches first", 'everything', '佛教', 2200, 2300, /(佛教|仏教)/, 100
+      # modern 仏教
+      it_behaves_like "result size and vern short title matches first", 'everything', '仏教', 2200, 2300, /(佛教|仏教)/, 100
+      exact_245a = ['6854317', '4162614', '6276328', '10243029', '10243045', '10243039']
+      it_behaves_like "matches in vern short titles first", 'everything', '仏教', /^(佛教|仏教)[^[[:alnum:]]]*$/, 5  # exact title match
+      context "w lang limit" do
         # trad
-        it_behaves_like "result size and vern short title matches first", 'everything', '佛教', 2200, 2300, /(佛教|仏教)/, 100
+        it_behaves_like "result size and vern short title matches first", 'everything', '佛教', 1000, 1100, /(佛教|仏教)/, 100, lang_limit
         # modern
-        it_behaves_like "result size and vern short title matches first", 'everything', '仏教', 2200, 2300, /(佛教|仏教)/, 100
-        exact_245a = ['6854317', '4162614', '6276328', '10243029', '10243045', '10243039']
-        it_behaves_like "matches in vern short titles first", 'everything', '仏教', /^(佛教|仏教)[^[[:alnum:]]]*$/, 5  # exact title match
-        context "w lang limit" do
-          # trad
-          it_behaves_like "result size and vern short title matches first", 'everything', '佛教', 1000, 1100, /(佛教|仏教)/, 100, lang_limit
-          # modern
-          it_behaves_like "result size and vern short title matches first", 'everything', '仏教', 1000, 1100, /(佛教|仏教)/, 100, lang_limit
-        end
+        it_behaves_like "result size and vern short title matches first", 'everything', '仏教', 1000, 1100, /(佛教|仏教)/, 100, lang_limit
+      end
+    end # buddhism
+
+    context 'Edo (old name for Tokyo)', :jira => ['VUF-2726', 'VUF-2770'] do
+      # Second char of traditional doesn't translate to second char of modern with ICU traditional->simplified 
+      it_behaves_like "both scripts get expected result size", 'everything', 'traditional', '江戶', 'modern', '江戸', 1900, 2000
+      # trad  江戶
+      it_behaves_like "result size and vern short title matches first", 'everything', '江戶', 1900, 2000, /(江戶|江戸)/, 100
+      # modern  江戸
+      it_behaves_like "expected result size", 'everything', '江戸', 3, 5
+      context "w lang limit" do
+        # trad
+        it_behaves_like "result size and vern short title matches first", 'everything', '江戶', 1900, 1950, /江戶/, 100, lang_limit
+        # modern
+        it_behaves_like "expected result size", 'everything', '江戸', 3, 5, lang_limit
       end
     end
-  end
+
+
+  end # modern Kanji != simplified Han
+
+
 
 end
