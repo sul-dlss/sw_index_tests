d584c419e8549a262a041a5f3d014a5ab3a74650
Naomi Dushay <ndushay@stanford.edu>
2013-09-24 16:11:53 -0700

    added three kingdoms to unigram spec as there is a bigram followed by a unigram

--- spec/cjk/chinese_unigram_spec.rb
+++ spec/cjk/chinese_unigram_spec.rb
@@ -5,11 +5,44 @@ describe "Chinese Unigrams", :chinese => true do
   
   context "Gone with the Wind", :jira => 'VUF-2789' do
     it_behaves_like "result size and vern short title matches first", 'title', '飘', 120, 130, /(飘|飄)/, 2
+    it_behaves_like "best matches first", 'title', '飘', '6701323', 5 # book
+    it_behaves_like "best matches first", 'title', '飘', '7737681', 5 # video
   end
 
   context "Zen", :jira => 'VUF-2790' do
     it_behaves_like "result size and vern short title matches first", 'title', '禪', 900, 1100, /(禪|禅)/, 100
-    it_behaves_like "both scripts get expected result size", 'title', 'traditional', '禪', 'modern', '禅', 900, 1100
+    it_behaves_like "both scripts get expected result size", 'title', 'traditional', '禪', 'simplified', '禅', 900, 1100
+  end
+  
+  context "bigram + unigram" do
+    context "Three kingdoms 三國誌" do
+      # 三國 (three - bigram) and  誌 (kingdom unigram)
+      # San guo zhi by Chen Shou  (exact 245a)
+      chinese_245a = ['6696113', # microform, EAL
+                      '9265099', # v. 394  harvard-yenching coll, sal1&2
+                      '5630782', # v1-5  EAL Chinese coll
+                      '9424180', # v 487  harvard-yenching coll, sal1&2
+                      '8656531', # ser2: v14  sal1&2
+                      '8656623', # ser2:v106 sal1&2
+                      '9407164', # ser2:v53 harvard-yenching coll, sal1&2
+                      '6718129', # v254 east asia big sets sal1&2
+                      '6435402', # 65 juan  v1-5  harvard-yenching coll, sal1&2
+                      '6829978', # 65 juan  v1-4  EAL chinese coll
+                      '10102192', # v96-97  sal1&2
+                      '8656532',  # ser2 v15  sal1&2
+                      '6435401'   # v1-16  harvard-yenching coll, sal1&2
+                      ]
+      korean_245a = ['8303152', # Samgukchi / Na Kwan-jung chiŭm  author Luo, Guanzhong  (245a, first 3 chars of 240a, 700t)
+                      '10156316' # Samgukchi by  Yi Mun-yŏl p'yŏngyŏk  (245a, part of 246a, 500a, 700t)
+                    ]
+      it_behaves_like "good results for query", 'title', '三國誌', 170, 200, chinese_245a, 20
+      it_behaves_like "good results for query", 'title', '三國 誌', 170, 200, chinese_245a, 25, 'rows' => 25
+      it_behaves_like "result size and vern short title matches first", 'title', '三國誌', 170, 200, /(三國誌|三国志|三國志)/, 30, 'rows' => 50
+      it_behaves_like "result size and vern short title matches first", 'title', '三国 志', 170, 200, /(三國誌|三国志|三國志)/, 30, 'rows' => 50
+      it_behaves_like "best matches first", 'title', '三國誌', korean_245a, 20
+      it_behaves_like "best matches first", 'title', '三國 誌', korean_245a, 20
+      it_behaves_like "both scripts get expected result size", 'title', 'traditional', '三國誌', 'simplified', '三国志', 170, 200
+    end
   end
   
 end
