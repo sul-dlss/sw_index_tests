7802435b18ad0c68e0ba1b340ca0893a40001029
Naomi Dushay <ndushay@stanford.edu>
2013-09-24 11:39:47 -0700

    beefed up japanese unigram specs per older version of char translations spec

--- spec/cjk/japanese_unigram_spec.rb
+++ spec/cjk/japanese_unigram_spec.rb
@@ -5,14 +5,26 @@ describe "Japanese Unigrams", :japanese => true do
   
   # TODO:  want  hiragana, katakana
   
-  context "Ran  乱 (modern)" do
+  lang_limit = {'fq'=>'language:Japanese'}
+
+  context "Ran (Kurosawa movie)" do
     it_behaves_like "result size and vern short title matches first", 'title', '乱', 725, 800, /乱/, 4
-    it_behaves_like "best matches first", 'title', '乱', '6260985', 4
+    it_behaves_like "both scripts get expected result size", 'title', 'traditional', '亂', 'modern', '乱', 725, 800
+    it_behaves_like "both scripts get expected result size", 'title', 'traditional', '亂', 'modern', '乱', 350, 400, lang_limit
+    # 6260985 - modern;  4176905 - trad
+    it_behaves_like "best matches first", 'title', '乱', ['6260985', '4176905'], 6
+    it_behaves_like "best matches first", 'title', '亂', ['6260985', '4176905'], 6
   end
 
-  context "Zen  禅 (modern)" do
+  context "Zen" do
     it_behaves_like "result size and vern short title matches first", 'title', '禅', 900, 1100, /禅/, 6
-    it_behaves_like "best matches first", 'title', '禅', '4193363', 6
+    it_behaves_like "both scripts get expected result size", 'title', 'traditional', '禪', 'modern', '禅', 900, 1100
+    it_behaves_like "both scripts get expected result size", 'title', 'traditional', '禪', 'modern', '禅', 425, 500, lang_limit
+    # 4193363 - modern;  6667691 - trad
+    it_behaves_like "best matches first", 'title', '禅', ['4193363', '6667691'], 6, lang_limit
+    # FIXME:  interesting that the sort order changes ...
+#    it_behaves_like "best matches first", 'title', '禪', ['4193363', '6667691'], 50, lang_limit
+    it_behaves_like "best matches first", 'title', '禪', '6667691', 6, lang_limit
   end
   
 end
