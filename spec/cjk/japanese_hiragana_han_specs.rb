035dc0c65a33348de989437f2975d4c821d11b74
Naomi Dushay <ndushay@stanford.edu>
2013-09-23 17:51:12 -0700

    add skeleton of japanese hiragana <-> han spec

---
+++ spec/cjk/japanese_hiragana_han_specs.rb
@@ -0,0 +1,20 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe "Japanese Hiragana-Han translations", :japanese => true, :fixme => true do
+
+  lang_limit = {'fq'=>'language:Japanese'}
+
+  # TODO:  we have no such translations at this time
+
+  context "personal name (hiragana)  けいちゅう", :jira => 'VUF-2772' do
+    # "There are several kanji conversions, such as ”けいちゅう”　<=>  契沖,  傾注　 景中,  刑中, etc.,
+    # and if "けいちゅう" retrieves all that, the search results may be too inclusive & not useful"
+    it_behaves_like "both scripts get expected result size", 'author', 'hiragana', 'けいちゅう', 'kanji', '契沖', 5, 5
+    context "hiragana  けいちゅう", :fixme => true do
+      it_behaves_like "expected result size", 'author', 'けいちゅう', 0, 5
+      it_behaves_like "does not find irrelevant results", 'author', 'けいちゅう', '4227249'
+    end
+  end
+
+end
