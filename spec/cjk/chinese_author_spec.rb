04fa4ed93f49acb07900bd52efb151821723c2b5
Naomi Dushay <ndushay@stanford.edu>
2013-09-24 17:42:56 -0700

    implemented chinese author specs

--- spec/cjk/chinese_author_spec.rb
+++ spec/cjk/chinese_author_spec.rb
@@ -27,13 +27,31 @@ describe "Chinese Author", :chinese => true do
     it_behaves_like "best matches first", 'author', '方宝川', simp, 8
   end
 
-  context "Zhang, Ailing" do
-    # simplified: 张爱玲 vs. traditional: 張愛玲 
-    
+  context "Liang, Hong", :jira => 'VUF-2688' do
+    it_behaves_like "both scripts get expected result size", 'author', 'traditional', '梁鴻', 'simplified', '梁鸿', 8, 12
+    it_behaves_like "matches in vern person authors first", 'author', '梁鸿', /(梁鴻|梁鸿)/, 7
+    it_behaves_like "matches in vern person authors first", 'author', '梁鸿', /^(梁鴻|梁鸿)[^[[:alpha:]]]*$/, 3
+    context "phrase" do
+      it_behaves_like "both scripts get expected result size", 'author', 'traditional', '"梁鴻"', 'simplified', '"梁鸿"', 8, 12
+      it_behaves_like "matches in vern person authors first", 'author', '"梁鸿"', /(梁鴻|梁鸿)/, 7
+      it_behaves_like "matches in vern person authors first", 'author', '"梁鸿"', /^(梁鴻|梁鸿)[^[[:alpha:]]]*$/, 3
+    end
   end
 
-  context "Shao, Dongfang" do
+  context "Liang, Shiqiu" do
+    it_behaves_like "both scripts get expected result size", 'author', 'traditional', '梁實秋', 'simplified', '梁实秋', 75, 90
+    it_behaves_like "matches in vern person authors first", 'author', '梁實秋', /^(梁實秋|梁实秋)[^[[:alpha:]]]*$/, 35
+  end
+
+  context "Shao, Dongfang", :jira => 'SW-207' do
     # simplified 邵东方  vs. traditional 邵東方  
+    it_behaves_like "both scripts get expected result size", 'author', 'traditional', '邵東方', 'simplified', '邵东方', 6, 15
+    it_behaves_like "matches in vern person authors first", 'author', '邵東方', /^(邵東方|邵东方)[^[[:alpha:]]]*$/, 6
+  end
+
+  context "Zhang, Ailing", :jira => 'SW-207' do
+    it_behaves_like "both scripts get expected result size", 'author', 'traditional', '張愛玲', 'simplified', '张爱玲', 75, 90
+    it_behaves_like "matches in vern person authors first", 'author', '張愛玲', /^(張愛玲|张爱玲)[^[[:alpha:]]]*$/, 50
   end
 
 end
