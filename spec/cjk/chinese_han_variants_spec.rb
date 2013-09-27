bfae398a0521af2a66dac1a44db49fb9a6dda3aa
Naomi Dushay <ndushay@stanford.edu>
2013-09-27 12:19:45 -0700

    add 2 chinese han variant specs

--- spec/cjk/chinese_han_variants_spec.rb
+++ spec/cjk/chinese_han_variants_spec.rb
@@ -21,6 +21,28 @@ describe "Chinese Han variants", :chinese => true, :fixme => true do
     end
   end
   
+  context "Hiroshi", :jira => 'VUF-2760' do
+    #  廣甯縣 
+    # desire:  first and second chars 廣寧   become  广宁
+    # first char  廣 (U+5EE3) => becomes  广 (U+5E7F)
+    # second char 甯 (U+752F) => becomes  甯 (same: U+752F), not  宁 (U+5B81)
+    orig_query_results = ['10215287', # in 505
+                          '6540140', #  unclear -- not relevant?
+                          ]
+    addl_desired_results = ['4184963', # 245a
+                            '6833733', # 245a
+                          ]
+    shared_examples_for "great matches for Hiroshi" do | query |
+      it_behaves_like "best matches first", 'title', query, orig_query_results, 5
+      it_behaves_like "best matches first", 'title', query, addl_desired_results, 5
+    end
+    trad = '廣甯縣'
+    simp = '广宁县'
+    it_behaves_like "both scripts get expected result size", 'title', 'traditional', trad, 'simplified', simp, 25, 35
+    it_behaves_like "great matches for Hiroshi", trad
+    it_behaves_like "great matches for Hiroshi", simp
+  end
+  
   context "history research" do
     # the 3rd character
     #  历史硏究   硏  U+784F   in the records   6433575, 9336336
@@ -55,6 +77,21 @@ describe "Chinese Han variants", :chinese => true, :fixme => true do
     it_behaves_like "both scripts get expected result size", 'title', 'traditional', '國際政治硏究', 'simplified', '国际政治研究', 24, 40
     it_behaves_like "best matches first", 'title', '國際政治硏究', '7106961', 1
     it_behaves_like "best matches first", 'title', '国际政治研究', '7106961', 1
+  end
+  
+  context "xi ju yan jiu", :jira => 'VUF-2798' do
+    #  戲 (U+6232)  which can be  戯 (U+622F), 戱 (U+6231), 戲 (U+6232) or 戏 (U+620F)
+    #  研 (U+7814) which can be 研 (U+7814) or 硏 (U+784F)
+    # 
+    it "戲劇研究" do
+      it_behaves_like "great matches for 戲劇研究", 'title', '戲劇研究', 
+        ['6694075', # 245a 戲劇研究
+          '9669954', # 245a  戯劇硏究
+          '6694086', # 245a 戯劇硏究
+          '6860183', # 245a  戏剧硏究
+          ], 
+          4, {'fq'=> 'format:Journal/Periodical'}
+    end
   end
 
   context "Yue Fu Zhi", :jira => 'VUF-2746' do
