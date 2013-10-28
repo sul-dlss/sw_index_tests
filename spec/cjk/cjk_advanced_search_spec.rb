5c9769fa67379c49c6e036ed96b0405a29945b6c
Naomi Dushay <ndushay@stanford.edu>
2013-10-28 11:35:38 -0700

    cjk advanced search - fix mm setting with local args, and add a summary search example

--- spec/cjk/cjk_advanced_search_spec.rb
+++ spec/cjk/cjk_advanced_search_spec.rb
@@ -2,6 +2,10 @@
 require 'spec_helper'
 
 describe "CJK Advanced Search" do
+  # in a non-CJK aware world, the only matches are ones where the whitespace matches.
+  # in a CJK-aware world, matches also include word boundaries that aren't denoted by whitespace
+  # in a CJK-aware advanced search, we should get matches that are only in the desired fields 
+  #   AND the matches shouldn't require whitespace for word boundaries
   
   context "Publication Info" do
     context "Publisher: Akatsuki Shobō" do
@@ -13,11 +17,11 @@ describe "CJK Advanced Search" do
         @resp.should have_at_least(3).documents
         @resp.should have_at_most(10).documents  # 41 match everything search
       end
-      it "exact matches first" do
+      it "whitespace exact matches first" do
         exact_matches = ['6321193', '6355327']
-        @resp.should include(exact_matches).in_first(exact_matches.size).documents
+        @resp.should include(exact_matches).in_first(exact_matches.size + 2).documents
       end
-      it "inexact matches present" do
+      it "matches without spaces present" do
         @resp.should include('6668315').in_first(20).documents
       end
     end
@@ -31,7 +35,7 @@ describe "CJK Advanced Search" do
         @resp.should have_at_least(800).documents
         @resp.should have_at_most(914).documents  # 915 match everything search
       end
-      it "exact matches first" do
+      it "whitespace exact matches first" do
         exact_matches = ['4196577', '4203788', '4199853', '4198109', '4203994', '4197487']
         @resp.should include(exact_matches).in_first(exact_matches.size).documents
       end
@@ -51,10 +55,10 @@ describe "CJK Advanced Search" do
         @resp.should have_at_least(15).documents
         @resp.should have_at_most(16).documents # 17 match everything search
       end
-      it "exact matches first" do
+      it "whitespace exact matches first" do
         @resp.should include(exact_matches).in_first(exact_matches.size).documents
       end
-      it "inexact matches present" do
+      it "matches without spaces present" do
         inexact_matches = '8630944'
         @resp.should include(inexact_matches).in_first(20).documents
       end
@@ -81,6 +85,29 @@ describe "CJK Advanced Search" do
 =end    
   end # Publication Info
   
+  context "Summary/ToC" do
+    context "ToC: Haiku futabashū" do
+      before(:all) do
+        @resp = cjk_adv_solr_resp({'q'=>"#{cjk_summary_query('俳句二葉集')}"}.merge(solr_args))
+      end
+      it "num expected" do
+        # there is only 1 exact match as of 2013-10-25; these are the only ones found w/o cjk search fields
+        @resp.should have_at_least(1).documents
+        @resp.should have_at_most(5).documents  # 3 match everything search
+      end
+      it "whitespace exact matches first" do
+        exact_matches = ['6626123']
+        @resp.should include(exact_matches).in_first(exact_matches.size).documents
+      end
+      it "matches without spaces present" do
+        no_space_exact_matches = ['6305856', '6626759'] 
+        @resp.should include(no_space_exact_matches).in_first(5).documents
+      end
+    end
+  end
+  
+  
+private  
   
   # send a GET request to the indicated Solr request handler with the indicated Solr parameters
   # @param solr_params [Hash] the key/value pairs to be sent to Solr as HTTP parameters
@@ -91,10 +118,14 @@ describe "CJK Advanced Search" do
     if num_cjk_uni(q_val) == 0
       RSpecSolr::SolrResponseHash.new(solr_conn.send_and_receive(req_handler, {:method => :get, :params => solr_params.merge(solr_args)}))
     else
-      RSpecSolr::SolrResponseHash.new(solr_conn.send_and_receive(req_handler, {:method => :get, :params => solr_params.merge(solr_args).merge(cjk_mm_qs_params(q_val))}))
-    end # have CJK in query
+      if q_val =~ /\{(.*)\}(.*)/
+        qf_pf_args = $1
+        terms = $2
+      end
+      RSpecSolr::SolrResponseHash.new(solr_conn.send_and_receive(req_handler, {:method => :get, 
+        :params => solr_params.merge(solr_args).merge(cjk_mm_qs_params(terms ? terms : q_val))}))
+    end
   end
-  
   
   def cjk_title_query terms
     '_query_:"{!edismax qf=$qf_title_cjk pf=$pf_title_cjk pf3=$pf3_title_cjk pf2=$pf2_title_cjk}' + terms + '"'
@@ -120,6 +151,4 @@ describe "CJK Advanced Search" do
   def solr_args
     {"defType"=>"lucene", "testing"=>"sw_index_test"}.merge(doc_ids_only)
   end
-  
-  
-end
+end # describe CJK Advanced Search
