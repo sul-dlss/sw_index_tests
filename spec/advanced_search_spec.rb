2ea3cee3b3baf7e5c8a9f530156c522deb96978f
Naomi Dushay <ndushay@stanford.edu>
2013-12-30 14:25:37 -0800

    fix two failing advanced search specs due to more records added

--- spec/advanced_search_spec.rb
+++ spec/advanced_search_spec.rb
@@ -336,12 +336,12 @@ describe "advanced search" do
       it "pub info 2010" do
         resp = solr_resp_doc_ids_only({'q'=>"#{pub_info_query('2010')}"}.merge(solr_args))
         resp.should have_at_least(127000).results
-        resp.should have_at_most(130600).results
+        resp.should have_at_most(131650).results
       end
       it "pub info 2011" do
         resp = solr_resp_doc_ids_only({'q'=>"#{pub_info_query('2011')}"}.merge(solr_args))
         resp.should have_at_least(119000).results
-        resp.should have_at_most(120100).results
+        resp.should have_at_most(120800).results
       end
       it "subject and pub info 2010" do
         resp = solr_resp_doc_ids_only({'q'=>"#{subject_query('soviet union and historiography')} AND #{pub_info_query('2010')}"}.merge(solr_args))
