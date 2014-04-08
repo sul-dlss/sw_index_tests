ec384d8d832ebbc3e1cd69fd3e143fae5b9f4562
Naomi Dushay <ndushay@stanford.edu>
2014-04-08 10:38:36 -0700

    fix Japanese Title searches buddhism behaves like both scripts get expected result size title search due to more records added

--- spec/cjk/japanese_title_spec.rb
+++ spec/cjk/japanese_title_spec.rb
@@ -13,7 +13,7 @@ describe "Japanese Title searches", :japanese => true do
     # First char of traditional doesn't translate to first char of modern with ICU traditional->simplified 
     # (traditional and simplified are the same;  modern is different)
     # (see also japanese han variants spec)
-    it_behaves_like "both scripts get expected result size", 'title', 'traditional', '佛教', 'modern', '仏教', 1900, 2700
+    it_behaves_like "both scripts get expected result size", 'title', 'traditional', '佛教', 'modern', '仏教', 2500, 2900
     it_behaves_like "matches in vern short titles first", 'title', '佛教', /(佛|仏)(教|敎)/, 100  # trad
     it_behaves_like "matches in vern short titles first", 'title', '仏教', /(佛|仏)(教|敎)/, 100  # modern
     exact_245a = ['6854317', '4162614', '6276328', '10243029', '10243045', '10243039']
