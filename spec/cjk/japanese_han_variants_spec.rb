fecf9137b9a2d999b33d93817a671aae452e00bc
Naomi Dushay <ndushay@stanford.edu>
2014-04-16 10:35:39 -0700

    fix japanese han variants buddhism spec failure due to more records added

--- spec/cjk/japanese_han_variants_spec.rb
+++ spec/cjk/japanese_han_variants_spec.rb
@@ -10,7 +10,7 @@ describe "Japanese Kanji variants", :japanese => true do
       # First char of traditional doesn't translate to first char of modern with ICU traditional->simplified 
       # (traditional and simplified are the same;  modern is different)
       # second char also has variant:   敎 654E (variant) => 教 6559 (std trad)
-      it_behaves_like "both scripts get expected result size", 'everything', 'traditional', '佛教', 'modern', '仏教', 2200, 3100
+      it_behaves_like "both scripts get expected result size", 'everything', 'traditional', '佛教', 'modern', '仏教', 2200, 3200
       it_behaves_like "matches in vern short titles first", 'everything', '佛教', /(佛|仏)(教|敎)/, 100  # trad
       it_behaves_like "matches in vern short titles first", 'everything', '仏教', /(佛|仏)(教|敎)/, 100  # modern
       exact_245a = ['6854317', '4162614', '6276328', '10243029', '10243045', '10243039']
@@ -18,9 +18,9 @@ describe "Japanese Kanji variants", :japanese => true do
       it_behaves_like "matches in vern short titles first", 'title', '仏教', /^(佛|仏)(教|敎).*$/, 7 # title starts w match
       context "w lang limit" do
         # trad
-        it_behaves_like "result size and vern short title matches first", 'everything', '佛教', 1000, 1300, /(佛|仏)(教|敎)/, 100, lang_limit
+        it_behaves_like "result size and vern short title matches first", 'everything', '佛教', 1000, 1350, /(佛|仏)(教|敎)/, 100, lang_limit
         # modern
-        it_behaves_like "result size and vern short title matches first", 'everything', '仏教', 1000, 1300, /(佛|仏)(教|敎)/, 100, lang_limit
+        it_behaves_like "result size and vern short title matches first", 'everything', '仏教', 1000, 1350, /(佛|仏)(教|敎)/, 100, lang_limit
       end
     end # buddhism
 
