5951b45d676798c25c4f52a0a39e0900b9fcff98
Laney McGlohon <laneymcg@stanford.edu>
2014-05-23 08:51:47 -0700

    SynonymFilterFactory musical keys sharp keys c# minor: increased limit

--- spec/synonym_spec.rb
+++ spec/synonym_spec.rb
@@ -177,7 +177,7 @@ describe "Tests for synonyms.txt used by Solr SynonymFilterFactory" do
       it "c# minor" do
         resp = solr_response({'q' => 'c# minor', 'fl'=>'id,title_display', 'facet'=>false})
         resp.should include("title_display" => /c(#|♯|\-sharp| sharp) minor/i).in_each_of_first(20).documents
-        resp.should have_at_most(1725).documents
+        resp.should have_at_most(1750).documents
         resp.should have_the_same_number_of_results_as(solr_resp_ids_from_query('C♯ minor'))
         resp.should have_the_same_number_of_results_as(solr_resp_ids_from_query('C-sharp minor'))
         # would also match  c ... minor ... sharp
