d92c5b2fcdd2cc77ceef73016fdcbb956bdd918b
Laney McGlohon <laneymcg@stanford.edu>
2014-06-30 11:30:54 -0700

    Fixed Series Search lecture notes in computer science: increased have_at_most limit

--- spec/series_search_spec.rb
+++ spec/series_search_spec.rb
@@ -7,7 +7,7 @@ describe "Series Search" do
   it "lecture notes in computer science" do
     resp = solr_resp_doc_ids_only(series_search_args 'lecture notes in computer science')
     resp.should have_at_least(7000).results
-    resp.should have_at_most(8000).results
+    resp.should have_at_most(8025).results
   end
   
   it "Lecture notes in statistics (Springer-Verlag)", :jira => 'VUF-1221' do
