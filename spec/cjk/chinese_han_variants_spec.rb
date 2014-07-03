f98eadf5c0a6d62e1d04c5e847d025f3c80d52f5
Laney McGlohon <laneymcg@stanford.edu>
2014-07-03 08:58:25 -0700

    Fixed Chinese Han variants 敎 654E (variant) => 教 6559 (std trad): increased have_at_most value

--- spec/cjk/chinese_han_variants_spec.rb
+++ spec/cjk/chinese_han_variants_spec.rb
@@ -125,8 +125,8 @@ describe "Chinese Han variants", :chinese => true do
   context "敎 654E (variant) => 教 6559 (std trad)" do
     # FIXME:  these do not give the same numbers of results.
     #it_behaves_like "both scripts get expected result size", 'title', 'variant', '敎育', 'std trad', '教育', 3000, 3500, {'fq'=>'language:Japanese'}
-    it_behaves_like "expected result size", 'title', '敎育', 3490, 3500, {'fq'=>'language:Japanese'}  # variant
-    it_behaves_like "expected result size", 'title', '教育', 3490, 3500, {'fq'=>'language:Japanese'}  # std trad
+    it_behaves_like "expected result size", 'title', '敎育', 3490, 3510, {'fq'=>'language:Japanese'}  # variant
+    it_behaves_like "expected result size", 'title', '教育', 3490, 3510, {'fq'=>'language:Japanese'}  # std trad
 
   end
   
