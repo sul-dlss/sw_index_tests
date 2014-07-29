d6d297fafcdc19dde07985d1a8c40e486d4382ef
Laney McGlohon <laneymcg@stanford.edu>
2014-07-29 10:04:50 -0700

    Fixed advanced search subject and keyword subject -congresses, keyword IEEE xplore keyword, advanced search pub info subject 'soviet union and historiography' and pub info '1910-1911 pub info 2010, advanced search pub info subject 'soviet union and historiography' and pub info '1910-1911 pub info 2011

--- spec/advanced_search_spec.rb
+++ spec/advanced_search_spec.rb
@@ -110,7 +110,7 @@ describe "advanced search" do
         resp = solr_resp_doc_ids_only({'q'=>"#{description_query('IEEE xplore')}"}.merge(solr_args))
         resp.should have_the_same_number_of_results_as(solr_resp_ids_from_query("IEEE Xplore"))
         resp.should have_at_least(7650).results
-        resp.should have_at_most(8600).results
+        resp.should have_at_most(8650).results
         resp.should have_fewer_results_than(solr_resp_doc_ids_only({'q'=>"#{description_query('IEEE OR xplore')}"}.merge(solr_args)))
       end
       it "subject NOT congresses and keyword" do
@@ -336,12 +336,12 @@ describe "advanced search" do
       it "pub info 2010" do
         resp = solr_resp_doc_ids_only({'q'=>"#{pub_info_query('2010')}"}.merge(solr_args))
         resp.should have_at_least(130000).results
-        resp.should have_at_most(136300).results
+        resp.should have_at_most(136400).results
       end
       it "pub info 2011" do
         resp = solr_resp_doc_ids_only({'q'=>"#{pub_info_query('2011')}"}.merge(solr_args))
         resp.should have_at_least(120000).results
-        resp.should have_at_most(125000).results
+        resp.should have_at_most(125100).results
       end
       it "subject and pub info 2010" do
         resp = solr_resp_doc_ids_only({'q'=>"#{subject_query('soviet union and historiography')} AND #{pub_info_query('2010')}"}.merge(solr_args))
