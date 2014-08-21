90d770492a54e91d49517c6fc81d1235374f8ca6
Naomi Dushay <ndushay@stanford.edu>
2014-08-21 13:47:06 -0700

    fix advanced search format video, location green, language english, topic feature films

--- spec/advanced_search_spec.rb
+++ spec/advanced_search_spec.rb
@@ -403,8 +403,8 @@ describe "advanced search" do
       end
       it "add topic feature films" do
         resp = solr_resp_doc_ids_only({'fq' => 'format:("Video"), language:("English"), building_facet:("Green"), topic_facet:("Feature films")', 'q'=>'collection:*'}.merge(solr_args))
-        resp.should have_at_least(14500).results
-        resp.should have_at_most(18000).results
+        resp.should have_at_least(17500).results
+        resp.should have_at_most(18100).results
       end
       it "add topic science fiction" do
         resp = solr_resp_doc_ids_only({'fq' => 'format:("Video"), language:("English"), building_facet:("Green"), topic_facet:("Feature films"), topic_facet:("Science fiction films")', 'q'=>'collection:*'}.merge(solr_args))
