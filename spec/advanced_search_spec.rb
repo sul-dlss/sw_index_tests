764bf118676fc122b4fb67371e432203d121947d
Naomi Dushay <ndushay@stanford.edu>
2014-08-21 13:41:55 -0700

    fix advanced search pub info 2011

--- spec/advanced_search_spec.rb
+++ spec/advanced_search_spec.rb
@@ -340,8 +340,8 @@ describe "advanced search" do
       end
       it "pub info 2011" do
         resp = solr_resp_doc_ids_only({'q'=>"#{pub_info_query('2011')}"}.merge(solr_args))
-        resp.should have_at_least(120000).results
-        resp.should have_at_most(125350).results
+        resp.should have_at_least(125000).results
+        resp.should have_at_most(125500).results
       end
       it "subject and pub info 2010" do
         resp = solr_resp_doc_ids_only({'q'=>"#{subject_query('soviet union and historiography')} AND #{pub_info_query('2010')}"}.merge(solr_args))
