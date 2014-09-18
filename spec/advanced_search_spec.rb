1bf7a848832a0d2441ddebb4c03e4bbcbbde6d68
Laney McGlohon <laneymcg@stanford.edu>
2014-09-18 08:41:56 -0700

    Fixed multiple failures by increasing have_at_most values

--- spec/advanced_search_spec.rb
+++ spec/advanced_search_spec.rb
@@ -337,7 +337,7 @@ describe "advanced search" do
       it "pub info 2010" do
         resp = solr_resp_doc_ids_only({'q'=>"#{pub_info_query('2010')}"}.merge(solr_args))
         resp.should have_at_least(136200).results
-        resp.should have_at_most(137000).results
+        resp.should have_at_most(137010).results
       end
       it "pub info 2011" do
         resp = solr_resp_doc_ids_only({'q'=>"#{pub_info_query('2011')}"}.merge(solr_args))
