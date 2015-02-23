b165bd88c2a8a60892fbc110720582a8412ab3b3
Laney McGlohon <laneymcg@stanford.edu>
2015-02-23 08:14:05 -0800

    Fixed have_at_most values and number of rows for sorting

--- spec/advanced_search_spec.rb
+++ spec/advanced_search_spec.rb
@@ -399,13 +399,13 @@ describe "advanced search" do
       end
       it "before topics selected" do
         resp = solr_resp_doc_ids_only({'fq' => 'format:("Video"), language:("English"), building_facet:("Green")', 'q'=>'collection:*'}.merge(solr_args))
-        resp.should have_at_least(35500).results
-        resp.should have_at_most(36000).results
+        resp.should have_at_least(35600).results
+        resp.should have_at_most(36100).results
       end
       it "add topic feature films" do
         resp = solr_resp_doc_ids_only({'fq' => 'format:("Video"), language:("English"), building_facet:("Green"), topic_facet:("Feature films")', 'q'=>'collection:*'}.merge(solr_args))
-        resp.should have_at_least(17700).results
-        resp.should have_at_most(18500).results
+        resp.should have_at_least(17750).results
+        resp.should have_at_most(18550).results
       end
       it "add topic science fiction" do
         resp = solr_resp_doc_ids_only({'fq' => 'format:("Video"), language:("English"), building_facet:("Green"), topic_facet:("Feature films"), topic_facet:("Science fiction films")', 'q'=>'collection:*'}.merge(solr_args))
