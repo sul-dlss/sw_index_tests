708b60baae6417291a228cab958ac43818cc44cb
Laney McGlohon <laneymcg@stanford.edu>
2015-08-14 16:08:45 -0700

    Fixed failure by increasing have-at-most value

--- spec/cjk/korean_unigram_spec.rb
+++ spec/cjk/korean_unigram_spec.rb
@@ -5,7 +5,7 @@ describe "Korean: Unigram Searches", :korean => true do
   # from email from Vitus, Aug 20, 2012
 
   context "title  창 (window)" do
-    it_behaves_like "expected result size", 'title', '창', 575, 750
+    it_behaves_like "expected result size", 'title', '창', 575, 800
     before(:all) do
       @resp = solr_response({'q'=>cjk_q_arg('title', '창'), 'fl'=>'id,vern_title_245a_display', 'facet'=>false} )
     end
@@ -18,7 +18,7 @@ describe "Korean: Unigram Searches", :korean => true do
       @resp.should include("7875201").as_first
     end
   end
-  
+
   context "title  꿈 (dream)" do
     it_behaves_like "expected result size", 'title', '꿈', 150, 250
     before(:all) do
@@ -28,5 +28,5 @@ describe "Korean: Unigram Searches", :korean => true do
       @resp.should include({'vern_title_245a_display' => /꿈/}).in_each_of_first(75).documents
     end
   end
-  
+
 end
