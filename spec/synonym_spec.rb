0a0270358408a15b90e6a57916b6f8ea4a4bc5b6
Shelley Doljack <sdoljack@stanford.edu>
2018-02-21 13:53:45 -0800

    synonym_spec: updated expected record.

--- spec/synonym_spec.rb
+++ spec/synonym_spec.rb
@@ -107,7 +107,8 @@ describe "Tests for synonyms.txt used by Solr SynonymFilterFactory" do
       end
       it "professional C++" do
         resp = solr_resp_ids_from_query('professional C++')
-        expect(resp).to include(['9612289', '9240287', '7534583', '8257317', '9801531']).in_first(10).results
+        # ckey 7534583 doesn't exist anymore; replaced with ckey 11728202
+        expect(resp).to include(['9612289', '9240287', '11728202', '8257317', '9801531']).in_first(10).results
         expect(resp.size).to be <= 150
         expect(resp).not_to have_the_same_number_of_results_as(solr_resp_ids_from_query "professional C")
       end
