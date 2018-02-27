398cd592ecba37882986f9761e1df5bf1abcc745
Shelley Doljack <sdoljack@stanford.edu>
2018-02-27 15:36:13 -0800

    author_title: replaced expected record with more relevant record.

--- spec/author_title_spec.rb
+++ spec/author_title_spec.rb
@@ -87,7 +87,7 @@ describe "Author-Title Search" do
     q = '"beethoven ludwig van 1770-1827 sonatas piano no. 14"' # op. 27, no. 2, C♯ minor;
     resp = solr_response(author_title_search_args(q).merge!({'fl'=>'id,author_person_display,title_245a_display', 'facet'=>false}))
     expect(resp.size).to be <= 400
-    expect(resp).to include('10791173')
+    expect(resp).to include('11516420') # replaced 10791173 with more relevant record
 #    resp.should include("author_person_display" => /Beethoven/i).in_each_of_first(6).documents
   end
 
