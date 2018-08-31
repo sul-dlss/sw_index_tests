c9aa4fc791c438914510fa6ba45cdf9059830386
Shelley Doljack <sdoljack@stanford.edu>
2018-08-31 11:16:14 -0700

    Title more relevant now that fewer results are found.

--- spec/default_req_handler_spec.rb
+++ spec/default_req_handler_spec.rb
@@ -115,8 +115,8 @@ describe 'Default Request Handler' do
   it 'Lectures on the Calculus of Variations and Optimal Control Theory', jira: 'VUF-1995' do
     resp = solr_resp_ids_titles_from_query 'Lectures on the Calculus of Variations and Optimal Control Theory'
     expect(resp).to include('560042').as_first
-    expect(resp).not_to include('title_245a_display' => /Shape optimization and optimal design/i)
-    expect(resp.size).to be <= 350
+    expect(resp).not_to include('title_245a_display' => /Shape optimization and optimal design/i).in_each_of_first(5)
+    expect(resp.size).to be <= 100
   end
 
   it 'death and taxes', jira: 'SW-721' do
