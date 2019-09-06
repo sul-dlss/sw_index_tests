ab7bbc6eee229910a6382b489e5e4d28f3d194a6
Chris Beer <cabeer@stanford.edu>
2019-09-06 07:39:51 -0700

    ckeys replaced

--- spec/synonym_spec.rb
+++ spec/synonym_spec.rb
@@ -104,7 +104,7 @@ describe "Tests for synonyms.txt used by Solr SynonymFilterFactory" do
       end
       it "professional C++" do
         resp = solr_resp_ids_from_query('professional C++')
-        expect(resp).to include(['9612289', '9240287', '11728202', '8257317', '12422871']).in_first(10).results
+        expect(resp).to include(['13195660', '13217525', '13033516', '12666104', '13183812']).in_first(10).results
         expect(resp.size).to be <= 150
         expect(resp).not_to have_the_same_number_of_results_as(solr_resp_ids_from_query "professional C")
       end
