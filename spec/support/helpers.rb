f270979358dfb582b22a37b648eb28b0df700afa
Chris Beer <chris@cbeer.info>
2024-02-01 10:19:55 -0800

    Update stemming tests to be a little more lenient

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -152,6 +152,26 @@ module Helpers
     end
   end
 
+  # @!method have_the_same_number_of_results_as
+  # Determine if the receiver (a Solr response object) has the same number of total documents as the expected RSpecSolr::SolrResponseHash
+  # NOTE:  this is about the TOTAL number of Solr documents matching the queries, not solely the number of docs in THESE responses
+  RSpec::Matchers.define :have_about_the_same_number_of_results_as do |expected_solr_resp_hash|
+    match do |actual_solr_resp_hash|
+      actual_solr_resp_hash.size * 0.99 <=  expected_solr_resp_hash.size &&
+      actual_solr_resp_hash.size * 1.01 >=  expected_solr_resp_hash.size
+    end
+
+    failure_message do |actual_solr_resp_hash|
+      "expected #{actual_solr_resp_hash.size} documents in Solr response #{expected_solr_resp_hash.num_docs_partial_output_str}"
+    end
+
+    failure_message_when_negated do |actual_solr_resp_hash|
+      "expected (not #{actual_solr_resp_hash.size}) documents in Solr response #{expected_solr_resp_hash.num_docs_partial_output_str}"
+    end
+
+    diffable
+  end
+
   private
 
   # send a GET request to the indicated Solr request handler with the indicated Solr parameters
